6fb16325cdf9a2a782bcacbfdfa21f882820d7f9
Barry Harris <44396066+barry65536@users.noreply.github.com>
2012-01-27 20:40:42 +0000

    JacKc added clone of X-Men (dumped by Bonky0013)

--- src/burn/drv/konami/d_xmen.cpp
+++ src/burn/drv/konami/d_xmen.cpp
@@ -746,6 +746,43 @@ struct BurnDriver BurnDrvXmen = {
 };
 
 
+// X-Men (4 Players ver ADA)
+
+static struct BurnRomInfo xmenaRomDesc[] = {
+	{ "065-ada04.10d",	0x020000, 0xb8276624, 1 | BRF_PRG | BRF_ESS }, //  0 68k Code
+	{ "065-ada05.10f",	0x020000, 0xc68582ad, 1 | BRF_PRG | BRF_ESS }, //  1
+	{ "065-a02.9d",		0x040000, 0xb31dc44c, 1 | BRF_PRG | BRF_ESS }, //  2
+	{ "065-a03.9f",		0x040000, 0x13842fe6, 1 | BRF_PRG | BRF_ESS }, //  3
+
+	{ "065-a01.6f",		0x020000, 0x147d3a4d, 2 | BRF_PRG | BRF_ESS }, //  4 Z80 Code
+
+	{ "065-a08.15l",	0x100000, 0x6b649aca, 3 | BRF_GRA },           //  5 Background Tiles
+	{ "065-a07.16l",	0x100000, 0xc5dc8fc4, 3 | BRF_GRA },           //  6
+
+	{ "065-a09.2h",		0x100000, 0xea05d52f, 4 | BRF_GRA },           //  7 Sprites
+	{ "065-a10.2l",		0x100000, 0x96b91802, 4 | BRF_GRA },           //  8
+	{ "065-a12.1h",		0x100000, 0x321ed07a, 4 | BRF_GRA },           //  9
+	{ "065-a11.1l",		0x100000, 0x46da948e, 4 | BRF_GRA },           // 10
+
+	{ "065-a06.1f",		0x200000, 0x5adbcee0, 5 | BRF_SND },           // 11 K054539 Samples
+
+	{ "xmen_ada.nv",  0x000080, 0xa77a3891, BRF_OPT },
+};
+
+STD_ROM_PICK(xmena)
+STD_ROM_FN(xmena)
+
+struct BurnDriver BurnDrvXmena = {
+	"xmena", "xmen", NULL, NULL, "1992",
+	"X-Men (4 Players ver ADA)\0", NULL, "Konami", "GX065",
+	NULL, NULL, NULL, NULL,
+	BDF_GAME_WORKING | BDF_CLONE, 4, HARDWARE_PREFIX_KONAMI, GBF_SCRFIGHT, 0,
+	NULL, xmenaRomInfo, xmenaRomName, NULL, NULL, XmenInputInfo, NULL,
+	DrvInit, DrvExit, DrvFrame, DrvDraw, DrvScan, &DrvRecalc, 0x800,
+	288, 224, 4, 3
+};
+
+
 // X-Men (4 Players ver EBA)
 
 static struct BurnRomInfo xmeneRomDesc[] = {

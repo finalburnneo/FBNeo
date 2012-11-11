8c643284cc149dff43f36aa73d7ad1661d33a01d
Barry Harris <44396066+barry65536@users.noreply.github.com>
2012-11-11 20:02:19 +0000

    Add clone of Sunset Riders (JacKc, hap, Dave F)

--- src/burn/drv/konami/d_tmnt.cpp
+++ src/burn/drv/konami/d_tmnt.cpp
@@ -1546,6 +1546,28 @@ static struct BurnRomInfo SsriderseaaRomDesc[] = {
 STD_ROM_PICK(Ssriderseaa)
 STD_ROM_FN(Ssriderseaa)
 
+static struct BurnRomInfo SsridersuabRomDesc[] = {
+	{ "064uab02.8e",        0x040000, 0xf1a3c548, BRF_ESS | BRF_PRG }, //  0	68000 Program Code
+	{ "064uab03.8g",        0x040000, 0x66a61287, BRF_ESS | BRF_PRG }, //  1
+	{ "064eab04.10e",       0x020000, 0xef2315bd, BRF_ESS | BRF_PRG }, //  2
+	{ "064eab05.10g",       0x020000, 0x51d6fbc4, BRF_ESS | BRF_PRG }, //  3
+	
+	{ "064e01.2f",          0x010000, 0x44b9bc52, BRF_ESS | BRF_PRG }, //  4	Z80 Program 
+	
+	{ "064e12.16k",         0x080000, 0xe2bdc619, BRF_GRA },	   //  5	Tiles
+	{ "064e11.12k",         0x080000, 0x2d8ca8b0, BRF_GRA },	   //  6
+	
+	{ "064e09.7l",          0x100000, 0x4160c372, BRF_GRA },	   //  7	Sprites
+	{ "064e07.3l",          0x100000, 0x64dd673c, BRF_GRA },	   //  8
+	
+	{ "064e06.1d",          0x100000, 0x59810df9, BRF_SND },	   //  9	K053260 Samples
+
+	{ "ssriders_uab.nv",  0x000080, 0xfe08b210, BRF_OPT },
+};
+
+STD_ROM_PICK(Ssridersuab)
+STD_ROM_FN(Ssridersuab)
+
 static struct BurnRomInfo SsridersuacRomDesc[] = {
 	{ "064uac02.8e",        0x040000, 0x870473b6, BRF_ESS | BRF_PRG }, //  0	68000 Program Code
 	{ "064uac03.8g",        0x040000, 0xeadf289a, BRF_ESS | BRF_PRG }, //  1
@@ -6399,6 +6421,16 @@ struct BurnDriver BurnDrvSsriderseaa = {
 	NULL, NULL, NULL, NULL,
 	BDF_GAME_WORKING | BDF_CLONE, 4, HARDWARE_KONAMI_68K_Z80, GBF_PLATFORM, 0,
 	NULL, SsriderseaaRomInfo, SsriderseaaRomName, NULL, NULL, Ssriders4psInputInfo, NULL,
+	SsridersInit, BlswhstlExit, SsridersFrame, NULL, SsridersScan,
+	NULL, 0x810, 288, 224, 4, 3
+};
+
+struct BurnDriver BurnDrvSsridersuab = {
+	"ssridersuab", "ssriders", NULL, NULL, "1991",
+	"Sunset Riders (4 Players ver. UAB)\0", NULL, "Konami", "GX064",
+	NULL, NULL, NULL, NULL,
+	BDF_GAME_WORKING | BDF_CLONE, 4, HARDWARE_KONAMI_68K_Z80, GBF_PLATFORM, 0,
+	NULL, SsridersuabRomInfo, SsridersuabRomName, NULL, NULL, Ssriders4pInputInfo, NULL,
 	SsridersInit, BlswhstlExit, SsridersFrame, NULL, SsridersScan,
 	NULL, 0x810, 288, 224, 4, 3
 };

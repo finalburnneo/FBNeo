5781886563aaf29bb42ce780f28ad51df5d2f868
jackchatelet <jackchatelet@f2e10478-e7fb-40a4-b0e2-024e28a34407>
2013-10-19 12:36:43 +0000

    Added Fire Shark (World, set 2) (ShouTime, The Dumping Union)

--- src/burn/drv/toaplan/d_samesame.cpp
+++ src/burn/drv/toaplan/d_samesame.cpp
@@ -975,11 +975,11 @@ static struct BurnSampleInfo samesameSampleDesc[] = {
 STD_SAMPLE_PICK(samesame)
 STD_SAMPLE_FN(samesame)
 
-// Fire Shark
+// Fire Shark (World, set 1)
 
 static struct BurnRomInfo fireshrkRomDesc[] = {
-	{ "09.bin",		0x08000, 0xf0c70e6f, BRF_PRG | BRF_ESS },    //  0 CPU #0 code
-	{ "10.bin",		0x08000, 0x9d253d77, BRF_PRG | BRF_ESS },    //  1
+	{ "09.bin",			0x08000, 0xf0c70e6f, BRF_PRG | BRF_ESS },    //  0 CPU #0 code
+	{ "10.bin",			0x08000, 0x9d253d77, BRF_PRG | BRF_ESS },    //  1
 	{ "o17_11ii.7j",	0x20000, 0x6beac378, BRF_PRG | BRF_ESS },    //  2
 	{ "o17_12ii.7l",	0x20000, 0x6adb6eb5, BRF_PRG | BRF_ESS },    //  3
 
@@ -1004,10 +1004,48 @@ STD_ROM_FN(fireshrk)
 
 struct BurnDriver BurnDrvFireshrk = {
 	"fireshrk", NULL, NULL, "fireshrk", "1990",
-	"Fire Shark\0", "No sound", "Toaplan", "Toaplan BCU-2 / FCU-2 based",
+	"Fire Shark (World, set 1)\0", "No sound", "Toaplan", "Toaplan BCU-2 / FCU-2 based",
 	NULL, NULL, NULL, NULL,
 	BDF_GAME_WORKING | TOA_ROTATE_GRAPHICS_CCW, 2, HARDWARE_TOAPLAN_RAIZING, GBF_VERSHOOT, 0,
 	NULL, fireshrkRomInfo, fireshrkRomName, samesameSampleInfo, samesameSampleName, SamesameInputInfo, FireshrkDIPInfo,
+	DrvInit, DrvExit, DrvFrame, DrvDraw, DrvScan, &ToaRecalcPalette, 0x800,
+	240, 320, 3, 4
+};
+
+
+// Fire Shark (World, set 2)
+
+static struct BurnRomInfo fireshrkaRomDesc[] = {
+	{ "o17_09ii.8j",	0x08000, 0xb60541ee, BRF_PRG | BRF_ESS },    //  0 CPU #0 code
+	{ "o17_10ii.8l",	0x08000, 0x96f5045e, BRF_PRG | BRF_ESS },    //  1
+	{ "o17_11ii.7j",	0x20000, 0x6beac378, BRF_PRG | BRF_ESS },    //  2
+	{ "o17_12ii.7l",	0x20000, 0x6adb6eb5, BRF_PRG | BRF_ESS },    //  3
+
+	{ "o17_05.12j",		0x20000, 0x565315f8, BRF_GRA },              //  4 Tile data
+	{ "o17_06.13j",		0x20000, 0x95262d4c, BRF_GRA },              //  5
+	{ "o17_07.12l",		0x20000, 0x4c4b735c, BRF_GRA },              //  6
+	{ "o17_08.13l",		0x20000, 0x95c6586c, BRF_GRA },              //  7
+
+	{ "o17_01.1d",		0x20000, 0xea12e491, BRF_GRA },              //  8
+	{ "o17_02.3d",		0x20000, 0x32a13a9f, BRF_GRA },              //  9
+	{ "o17_03.5d",		0x20000, 0x68723dc9, BRF_GRA },              // 10
+	{ "o17_04.7d",		0x20000, 0xfe0ecb13, BRF_GRA },              // 12
+
+	{ "prom14.25b",		0x00020, 0xbc88cced, BRF_GRA },              // 12 Sprite attribute PROM
+	{ "prom15.20c",		0x00020, 0xa1e17492, BRF_GRA },              // 13
+
+	{ "hd647180.017",	0x08000, 0x00000000, BRF_OPT | BRF_NODUMP }, // 14 Sound HD647180 code
+};
+
+STD_ROM_PICK(fireshrka)
+STD_ROM_FN(fireshrka)
+
+struct BurnDriver BurnDrvFireshrka = {
+	"fireshrka", "fireshrk", NULL, "fireshrk", "1989",
+	"Fire Shark (Wolrd, set 2)\0", "No sound", "Toaplan", "Toaplan BCU-2 / FCU-2 based",
+	NULL, NULL, NULL, NULL,
+	BDF_GAME_WORKING | BDF_CLONE | TOA_ROTATE_GRAPHICS_CCW, 2, HARDWARE_TOAPLAN_RAIZING, GBF_VERSHOOT, 0,
+	NULL, fireshrkaRomInfo, fireshrkaRomName, samesameSampleInfo, samesameSampleName, SamesameInputInfo, FireshrkDIPInfo,
 	DrvInit, DrvExit, DrvFrame, DrvDraw, DrvScan, &ToaRecalcPalette, 0x800,
 	240, 320, 3, 4
 };

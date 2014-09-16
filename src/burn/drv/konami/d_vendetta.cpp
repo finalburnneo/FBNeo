25dd71579072415da0c8413a2bf689bfc655ba19
iq_132 <1191709+iq132@users.noreply.github.com>
2014-09-16 19:29:36 +0000

    Keep JackC happy. :)

--- src/burn/drv/konami/d_vendetta.cpp
+++ src/burn/drv/konami/d_vendetta.cpp
@@ -1062,6 +1062,40 @@ struct BurnDriver BurnDrvVendetta2p = {
 };
 
 
+// Vendetta (World, 2 Players ver. EB-A?)
+
+static struct BurnRomInfo vendetta2pebaRomDesc[] = {
+	{ "081-eb-a01.17c",    0x040000, 0x8430bb52, 1 | BRF_PRG | BRF_ESS }, //  0 Konami Custom Code
+
+	{ "081b02",            0x010000, 0x4c604d9b, 2 | BRF_PRG | BRF_ESS }, //  1 Z80 Code
+
+	{ "081a09",            0x080000, 0xb4c777a9, 3 | BRF_GRA },           //  2 K052109 Tiles
+	{ "081a08",            0x080000, 0x272ac8d9, 3 | BRF_GRA },           //  3
+
+	{ "081a04",            0x100000, 0x464b9aa4, 4 | BRF_GRA },           //  4 K053247 Tiles
+	{ "081a05",            0x100000, 0x4e173759, 4 | BRF_GRA },           //  5
+	{ "081a06",            0x100000, 0xe9fe6d80, 4 | BRF_GRA },           //  6
+	{ "081a07",            0x100000, 0x8a22b29a, 4 | BRF_GRA },           //  7
+
+	{ "081a03",            0x100000, 0x14b6baea, 5 | BRF_SND },           //  8 K053260 Samples
+
+	{ "vendetta.nv",    0x000080, 0xfbac4e30, BRF_OPT },
+};
+
+STD_ROM_PICK(vendetta2peba)
+STD_ROM_FN(vendetta2peba)
+
+struct BurnDriver BurnDrvVendetta2peba = {
+    "vendetta2peba", "vendetta", NULL, NULL, "1991",
+    "Vendetta (World, 2 Players ver. EB-A?)\0", NULL, "Konami", "GX081",
+    NULL, NULL, NULL, NULL,
+    BDF_GAME_WORKING | BDF_CLONE, 2, HARDWARE_PREFIX_KONAMI, GBF_SCRFIGHT, 0,
+    NULL, vendetta2pebaRomInfo, vendetta2pebaRomName, NULL, NULL, VendettaInputInfo, NULL,
+    VendettaInit, DrvExit, DrvFrame, DrvDraw, DrvScan, &DrvRecalc, 0x800,
+    304, 224, 4, 3
+};
+
+
 // Vendetta (Asia 2 Players ver. U)
 
 static struct BurnRomInfo vendetta2puRomDesc[] = {

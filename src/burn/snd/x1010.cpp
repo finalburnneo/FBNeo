09c404302fa4b6e1ba0dba97205826b711f55a4a
dinkc64 <12570148+dinkc64@users.noreply.github.com>
2014-09-28 05:23:11 +0000

    un-super debug (x1010.cpp)

--- src/burn/snd/x1010.cpp
+++ src/burn/snd/x1010.cpp
@@ -147,7 +147,7 @@ void x1010_sound_update()
 	 				// Envelope one shot mode
 					if( (reg->status&4) != 0 || delta >= 0x80 ) {
 						reg->status &= 0xfe;					// Key off
-                                                bprintf(0, _T("--super debug!-- smp ended delta[%X]\n"), delta);
+                                                //bprintf(0, _T("--super debug!-- smp ended delta[%X]\n"), delta);
                                                 break;
 					}
 

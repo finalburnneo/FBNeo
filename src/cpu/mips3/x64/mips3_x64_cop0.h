37c7337a412bd646b4419e6f078011aa13c06eb6
Marcos Medeiros <1381933+zxmarcos@users.noreply.github.com>
2015-02-23 17:43:11 +0000

    mips3: X64 recompiler (COP0 drc)

---
+++ src/cpu/mips3/x64/mips3_x64_cop0.h
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2015, Marcos Medeiros
+ * Licensed under BSD 3-clause.
+ */
+#ifndef MIPS3_X64_COP0
+#define MIPS3_X64_COP0
+
+
+#include "../mips3.h"
+#include "mips3_x64_defs.h"
+#include "mips3_x64.h"
+#include "xbyak/xbyak.h"
+
+namespace mips
+{
+
+
+bool mips3_x64::compile_cop0(uint32_t opcode)
+{
+    bool result = false;
+
+    switch (RSNUM) {
+    // MFC0 rt, rd
+    case 0x00:
+        if (RTNUM) {
+            switch (RDNUM) {
+            // Hack: (total_cycles - reset_cycle) * 16
+            case COP0_Count:
+                sub(r15, 250);
+                mov(rax, TOTAL_x);
+                sub(rax, RSTCYC_x);
+                shl(rax, 4);
+                mov(RT_x, rax);
+                return false;
+
+            case COP0_Cause:
+                sub(r15, 250);
+                break;
+            }
+            mov(rax, COP0_x(RDNUM));
+            cdqe();
+            mov(RT_x, rax);
+        }
+        break;
+    default:
+        fallback(opcode, &mips3::cop0_execute);
+        break;
+    }
+    return result;
+}
+
+}
+
+#endif // MIPS3_X64_COP0
+

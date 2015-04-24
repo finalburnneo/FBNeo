b28adf2f1fa1da699a74f9c82a47c2858d0b6f00
dinkc64 <12570148+dinkc64@users.noreply.github.com>
2015-04-24 06:28:44 +0000

    d_m72: clear dac when not being written to, fixes distortion in second-to-last level of mr.heli, also applies to end of level1 in air duel

--- src/burn/drv/irem/d_m72.cpp
+++ src/burn/drv/irem/d_m72.cpp
@@ -2123,10 +2123,13 @@ static INT32 DrvFrame()
 					if (DrvSndROM[sample_address]) {
 						DACSignedWrite(0, DrvSndROM[sample_address]);
 						sample_address = (sample_address + 1) & 0x3ffff;
-						if (!DrvSndROM[sample_address]) {
-							DACWrite(0, 0); // Clear DAC output buffer at end of sample - fixes distortion in Air Duel
-						}
+						//if (!DrvSndROM[sample_address]) {
+						//	DACWrite(0, 0); // Clear DAC output buffer at end of sample - fixes distortion in Air Duel
+						//}
+					} else {
+						DACWrite(0, 0); // Clear DAC output buffer at end of sample - fixes distortion in Air Duel & second-to-last level of Mr. Heli
 					}
+
 				} else if (z80_nmi_enable == Z80_REAL_NMI) {
 					 ZetNmi();
 				}

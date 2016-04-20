100a30c39500306839e58dbe5eef973fc7823155
dinkc64 <12570148+dinkc64@users.noreply.github.com>
2016-04-20 03:52:41 +0000

    add soundcores tms36xx, phoenix sound custom

---
+++ src/burn/snd/phoenixsound.h
@@ -0,0 +1,6 @@
+void phoenix_sound_update(INT16 *buffer, int length);
+void phoenix_sound_control_a_w(INT32 address, UINT8 data);
+void phoenix_sound_control_b_w(INT32 address, UINT8 data);
+void phoenix_sound_init();
+void phoenix_sound_deinit();
+

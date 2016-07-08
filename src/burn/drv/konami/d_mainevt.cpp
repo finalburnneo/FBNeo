de99768b1f5b95dcc49c7c8d234d7df0fce02a44
dinkc64 <12570148+dinkc64@users.noreply.github.com>
2016-07-08 22:25:58 +0000

    derp

--- src/burn/drv/konami/d_mainevt.cpp
+++ src/burn/drv/konami/d_mainevt.cpp
@@ -1,11 +1,6 @@
 // FB Alpha The Main Event / Devastators driver module
 // Based on MAME driver by Bryan McPhail
 
-// Priority issue with Main Event's ropes.  nBurnLayer & 4 vs. sprites.
-// The sprite engine draws the ropes ontop of the player sprite properly, but the
-// ropes from the tile engine (see: nBurnLayer & 4) have a higher priority causing an issue.
-// (see game.)
-
 #include "tiles_generic.h"
 #include "z80_intf.h"
 #include "hd6309_intf.h"

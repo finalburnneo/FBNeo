d4fb596974b77a51ae80103c7b9ffb5d43daa9c9
dinkc64 <12570148+dinkc64@users.noreply.github.com>
2016-11-18 02:03:17 +0000

    fix soldividk

--- src/burn/drv/psikyo/d_psikyosh.cpp
+++ src/burn/drv/psikyo/d_psikyosh.cpp
@@ -873,8 +873,8 @@ struct BurnDriver BurnDrvSoldivid = {
 static INT32 SoldividkInit()
 {
 	speedhack_address = 0x00000c;
-	speedhack_pc[0] = 0x0001AFDC;
-	speedhack_pc[1] = 0x0001AEA6;
+	speedhack_pc[0] = 0x0001AFB0;
+	speedhack_pc[1] = 0x0001AE7A;
 
 	return DrvInit(SoldividLoadCallback, 0, 0x3800000, 0x2000000);
 }
@@ -898,9 +898,9 @@ static struct BurnRomInfo soldividkRomDesc[] = {
 STD_ROM_PICK(soldividk)
 STD_ROM_FN(soldividk)
 
-struct BurnDriverD BurnDrvSoldividk = {
+struct BurnDriver BurnDrvSoldividk = {
 	"soldividk", "soldivid", NULL, NULL, "1997",
-	"Sol Divide - The Sword Of Darkness (Korea)\0", "Sound is broken/Use parent!", "Psikyo", "PS3-V1",
+	"Sol Divide - The Sword Of Darkness (Korea)\0", NULL, "Psikyo", "PS3-V1",
 	NULL, NULL, NULL, NULL,
 	BDF_GAME_WORKING | BDF_CLONE | BDF_HISCORE_SUPPORTED, 2, HARDWARE_PSIKYO, GBF_HORSHOOT, 0,
 	NULL, soldividkRomInfo, soldividkRomName, NULL, NULL, Common3ButtonInputInfo, SoldividkDIPInfo,

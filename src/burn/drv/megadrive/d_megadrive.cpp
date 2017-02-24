ae270074b923e6dc57324b2f575f38d707eb9232
dinkc64 <12570148+dinkc64@users.noreply.github.com>
2017-02-24 03:10:55 +0000

    megadrive: miniplanets updated to REV02

--- src/burn/drv/megadrive/d_megadrive.cpp
+++ src/burn/drv/megadrive/d_megadrive.cpp
@@ -41383,7 +41383,7 @@ struct BurnDriver BurnDrvmd_megaqbert11 = {
 
 // Mini Planets
 static struct BurnRomInfo md_miniplanetsRomDesc[] = {
-	{ "Miniplanets.bin", 0x40000, 0x934c81b8, BRF_PRG | SEGA_MD_ROM_LOAD16_WORD_SWAP | SEGA_MD_ROM_OFFS_000000  },
+	{ "Miniplanets-REV02.bin", 0x40000, 0x48460e4a, BRF_PRG | SEGA_MD_ROM_LOAD16_WORD_SWAP | SEGA_MD_ROM_OFFS_000000  },
 };
 
 STD_ROM_PICK(md_miniplanets)
@@ -41391,7 +41391,7 @@ STD_ROM_FN(md_miniplanets)
 
 struct BurnDriver BurnDrvmd_miniplanets = {
 	"md_miniplanets", NULL, NULL, NULL, "2016",
-	"Mini Planets\0", NULL, "2016 Sik", "Sega Megadrive",
+	"Mini Planets (REV02)\0", NULL, "2016 Sik", "Sega Megadrive",
 	NULL, NULL, NULL, NULL,
 	BDF_GAME_WORKING | BDF_16BIT_ONLY, 2, HARDWARE_SEGA_MEGADRIVE, GBF_MISC, 0,
 	MegadriveGetZipName, md_miniplanetsRomInfo, md_miniplanetsRomName, NULL, NULL, MegadriveInputInfo, MegadriveDIPInfo,

e6b9adc6d69d4ce3e41fbdac597a167e8d2bb936
Barry Harris <44396066+barry65536@users.noreply.github.com>
2017-03-11 22:08:09 +0000

    Visual Studio 2017 Support

--- src/dep/scripts/build_details.cpp
+++ src/dep/scripts/build_details.cpp
@@ -42,12 +42,14 @@ int main(int /*argc*/, char** /*argv*/)
   #else
 	printf("#define BUILD_COMP Visual C++ 2013\n");
   #endif
- #elif _MSC_VER >= 1900 && _MSC_VER < 2000
+ #elif _MSC_VER >= 1900 && _MSC_VER < 1910
   #if defined BUILD_VS_XP_TARGET
     printf("#define BUILD_COMP Visual C++ 2015 (XP)\n");
   #else
 	printf("#define BUILD_COMP Visual C++ 2015\n");
   #endif
+ #elif _MSC_VER >= 1910 && _MSC_VER < 1920
+    printf("#define BUILD_COMP Visual C++ 2017\n");
  #else
 	printf("#define BUILD_COMP Visual C++ %i.%i\n", _MSC_VER / 100 - 6, _MSC_VER % 100 / 10);
  #endif

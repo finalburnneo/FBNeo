099e78c4096da54e1038030d72f3d0966f182d1a
dinkc64 <12570148+dinkc64@users.noreply.github.com>
2017-08-11 01:47:27 +0000

    revert #4947, md: render at nBurnBpp aka remove reliance on DBF_16BIT_ONLY, now that BDF_16BIT_ONLY is fixed

--- src/burn/drv/megadrive/megadrive.cpp
+++ src/burn/drv/megadrive/megadrive.cpp
@@ -23,8 +23,7 @@
  Port by OopsWare overhaul by dink
  ********************************************************************************/
 
-//#include "burnint.h"
-#include "tiles_generic.h"
+#include "burnint.h"
 #include "m68000_intf.h"
 #include "z80_intf.h"
 #include "burn_md2612.h"
@@ -183,7 +182,7 @@ static UINT16 *MegadriveBackupRam;
 
 static UINT8 *HighCol;
 static UINT8 *HighColFull;
-static UINT8 *LineBuf;
+static UINT16 *LineBuf;
 
 static INT32 *HighCacheA;
 static INT32 *HighCacheB;
@@ -390,7 +389,7 @@ static INT32 MemIndex()
 	
 	HighColFull	= Next; Next += (8 + 320 + 8) * 240 + 1;
 
-	LineBuf     = (UINT8 *) Next; Next += 320 * 320 * sizeof(UINT32); // palete-processed line-buffer (dink / for sonic mode)
+	LineBuf     = (UINT16 *) Next; Next += 320 * 320 * sizeof(UINT16); // palete-processed line-buffer (dink / for sonic mode)
 	
 	HighCacheA	= (INT32 *) Next; Next += (41+1) * sizeof(INT32);	// caches for high layers
 	HighCacheB	= (INT32 *) Next; Next += (41+1) * sizeof(INT32);
@@ -3116,7 +3115,6 @@ INT32 MegadriveInit()
 	
 	// OSC_NTSC / 7
 	BurnSetRefreshRate(60.0);
-		GenericTilesInit();
 
 	bNoDebug = 0;
 	DrvSECAM = 0;
@@ -3162,7 +3160,7 @@ INT32 MegadriveExit()
 
 	BurnMD2612Exit();
 	SN76496Exit();
-	GenericTilesExit();
+	
 	if (Mem) {
 		BurnFree(Mem);
 		Mem = NULL;
@@ -4232,12 +4230,13 @@ static INT32 PicoLine(INT32 /*scan*/)
 		INT32 offset = 0;
 		if (!(RamVReg->reg[1] & 8)) offset = 8;
 
-		{ // render blank (BackFill()'d) line to previous line
-			UINT8 *pDest = LineBuf + (((Scanline-1) * 320) * nBurnBpp);
+		{ // copy blanked line to previous line
+			UINT16 *pDest = LineBuf + ((Scanline-1) * 320);
 			UINT8 *pSrc = HighColFull + (Scanline + offset)*(8+320+8) + 8;
 
 			for (INT32 i = 0; i < 320; i++)
-				PutPix(pDest + i * nBurnBpp, MegadriveCurPal[pSrc[i]]);
+				pDest[i] = MegadriveCurPal[pSrc[i]];
+
 		}
 	}
 	BlankedLine = 0;
@@ -4248,14 +4247,15 @@ static INT32 PicoLine(INT32 /*scan*/)
 	{
 		INT32 offset = 0;
 		if (!(RamVReg->reg[1] & 8)) offset = 8;
-		HighCol = HighColFull + ( (offset + Scanline + 1) * (8 + 320 + 8) ); // re: PicoFrameStart(); above: the SECOND line and following.. hence + 1
+		HighCol = HighColFull + ( (offset + Scanline + 1) * (8 + 320 + 8) ); // Set-up pointer to next line to be rendered to (see: PicoFrameStart();)
 
-		{ // render current line to linebuf, for mid-screen palette changes (referred to as SONIC rendering mode, for water & etc.)
-			UINT8 *pDest = LineBuf + ((Scanline * 320) * nBurnBpp);
+		{ // copy current line to linebuf, for mid-screen palette changes (referred to as SONIC rendering mode, for water & etc.)
+			UINT16 *pDest = LineBuf + (Scanline * 320);
 			UINT8 *pSrc = HighColFull + (Scanline + offset)*(8+320+8) + 8;
 
 			for (INT32 i = 0; i < 320; i++)
-				PutPix(pDest + i * nBurnBpp, MegadriveCurPal[pSrc[i]]);
+				pDest[i] = MegadriveCurPal[pSrc[i]];
+
 		}
 	}
 
@@ -4265,43 +4265,52 @@ static INT32 PicoLine(INT32 /*scan*/)
 INT32 MegadriveDraw()
 {
 	if (bMegadriveRecalcPalette) {
-		for (INT32 i=0; i< 0x40; i++)
+	    for (INT32 i=0; i< 0x40; i++)
 			CalcCol(i, BURN_ENDIAN_SWAP_INT16(RamPal[i]));
 		bMegadriveRecalcPalette = 0;
 	}
 
-	UINT8 *pDest = (UINT8 *)pBurnDraw;
+	UINT16 *pDest = (UINT16 *)pBurnDraw;
 
 	if ((RamVReg->reg[12]&1) || !(MegadriveDIP[1] & 0x03)) {
-		// Normal
+	
 		for (INT32 j=0; j < 224; j++) {
-			memcpy(pBurnDraw + ((j * 320) * nBurnBpp), LineBuf + ((j * 320) * nBurnBpp), 320 * nBurnBpp);
+			UINT16 *pSrc = LineBuf + (j * 320);
+			for (INT32 i = 0; i < 320; i++)
+				pDest[i] = pSrc[i];
+			pDest += 320;
 		}
+	
 	} else {
+		
 		if (( MegadriveDIP[1] & 0x03 ) == 0x01 ) {
-			// Center
-			pDest += 32 * nBurnBpp;
+			// Center 
+			pDest += 32;
 			for (INT32 j = 0; j < 224; j++) {
-				memset(pDest -  (32*nBurnBpp), 0, 32*nBurnBpp);
+				UINT16 *pSrc = LineBuf + (j * 320);
 
-				memcpy(pDest, LineBuf + ((j * 320) * nBurnBpp), 256 * nBurnBpp);
-
-				memset(pDest + (256*nBurnBpp), 0, 32*nBurnBpp);
-
-				pDest += 320 * nBurnBpp;
+				memset((UINT8 *)pDest -  32*2, 0, 64);
+				
+				for (INT32 i = 0; i < 256; i++)
+					pDest[i] = pSrc[i];
+				
+				memset((UINT8 *)pDest + 256*2, 0, 64);
+				
+				pDest += 320;
 			}
 		} else {
 			// Zoom
 			for (INT32 j = 0; j < 224; j++) {
-				UINT8 *pSrc = LineBuf + ((j * 320) * nBurnBpp);
+				UINT16 *pSrc = LineBuf + (j * 320);
 				UINT32 delta = 0;
 				for (INT32 i = 0; i < 320; i++) {
-					memcpy(&pDest[i * nBurnBpp], &pSrc[(delta >> 16) * nBurnBpp], nBurnBpp);
+					pDest[i] = pSrc[delta >> 16];
 					delta += 0xCCCC;
 				}
-				pDest += 320 * nBurnBpp;
+				pDest += 320;
 			}
 		}
+		
 	}
 
 	return 0;

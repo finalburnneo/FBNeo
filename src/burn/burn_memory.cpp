f632b1013b01c9c19a8d56605bee6187d7f39d0d
iq_132 <1191709+iq132@users.noreply.github.com>
2017-08-17 03:51:07 +0000

    Allow RAM usage.

--- src/burn/burn_memory.cpp
+++ src/burn/burn_memory.cpp
@@ -6,15 +6,21 @@
 
 #include "burnint.h"
 
+#define LOG_MEMORY_USASE
+
 #define MAX_MEM_PTR	0x400 // more than 1024 malloc calls should be insane...
 
 static UINT8 *memptr[MAX_MEM_PTR]; // pointer to allocated memory
+static INT32 memsize[MAX_MEM_PTR];
+static INT32 mem_allocated;
 
 // this should be called early on... BurnDrvInit?
 
 void BurnInitMemoryManager()
 {
-	memset (memptr, 0, MAX_MEM_PTR * sizeof(UINT8 **));	
+	memset (memptr, 0, MAX_MEM_PTR * sizeof(UINT8 **));
+	memset (memsize, 0, MAX_MEM_PTR * sizeof(INT32));
+	mem_allocated = 0;
 }
 
 // should we pass the pointer as a variable here so that we can save a pointer to it
@@ -34,6 +40,13 @@ UINT8 *BurnMalloc(INT32 size)
 			}
 
 			memset (memptr[i], 0, size); // set contents to 0
+
+			mem_allocated += size;
+			memsize[i] = size;
+
+#ifdef LOG_MEMORY_USASE
+			bprintf (0, _T("BurnMalloc allocated %d bytes of memory (%d bytes total allocated)!\n"), size, mem_allocated);
+#endif
 
 			return memptr[i];
 		}
@@ -55,6 +68,9 @@ void _BurnFree(void *ptr)
 			free (memptr[i]);
 			memptr[i] = NULL;
 
+			mem_allocated -= memsize[i];
+			memsize[i] = 0;
+
 			break;
 		}
 	}
@@ -68,10 +84,15 @@ void BurnExitMemoryManager()
 	{
 		if (memptr[i] != NULL) {
 #if defined FBA_DEBUG
-			bprintf(PRINT_ERROR, _T("BurnExitMemoryManager had to free mem pointer %i\n"), i);
+			bprintf(PRINT_ERROR, _T("BurnExitMemoryManager had to free mem pointer %i (%d bytes)\n"), i, memsize[i]);
 #endif
 			free (memptr[i]);
 			memptr[i] = NULL;
+
+			mem_allocated -= memsize[i];
+			memsize[i] = 0;
 		}
 	}
+
+	mem_allocated = 0;
 }

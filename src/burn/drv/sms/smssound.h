1c3f790e8a0b67863d587437a1ece61186c5451d
dinkc64 <12570148+dinkc64@users.noreply.github.com>
2018-02-02 01:18:47 +0000

    smssound: remove unneeded stuff as it causes problems in haiku os

--- src/burn/drv/sms/smssound.h
+++ src/burn/drv/sms/smssound.h
@@ -2,27 +2,10 @@
 #ifndef _SMSSOUND_H_
 #define _SMSSOUND_H_
 
-enum {
-    STREAM_PSG_L,               /* PSG left channel */
-    STREAM_PSG_R,               /* PSG right channel */
-    STREAM_FM_MO,               /* YM2413 melody channel */
-    STREAM_FM_RO,               /* YM2413 rhythm channel */
-    STREAM_MAX                  /* Total # of sound streams */
-};  
-
 /* Sound emulation structure */
 typedef struct
 {
-    void (*mixer_callback)(INT16 **stream, INT16 **output, INT32 length);
-    INT16 *output[2];
-    INT16 *stream[STREAM_MAX];
-    INT32 fm_which;
     INT32 enabled;
-    INT32 fps;
-    INT32 buffer_size;
-    INT32 sample_count;
-    INT32 sample_rate;
-    INT32 done_so_far;
     UINT32 fm_clock;
     UINT32 psg_clock;
 } snd_t;

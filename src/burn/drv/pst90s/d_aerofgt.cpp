36044a69a5c639f6e019f1c331de5a71f76d2c8b
jackchatelet <jackchatelet@f2e10478-e7fb-40a4-b0e2-024e28a34407>
2018-02-03 12:35:00 +0000

    d_aerofgt.cpp: added Turbo Force (World, set 2) [Corrado Tomaselli, The Dumping Union]

--- src/burn/drv/pst90s/d_aerofgt.cpp
+++ src/burn/drv/pst90s/d_aerofgt.cpp
@@ -2837,7 +2837,9 @@ struct BurnDriver BurnDrvAerofgt = {
 };
 
 
-// Turbo Force (World)
+// There is known to exist but not currently dumped a version of Turbo Force with the program roms stamped "7"
+
+// Turbo Force (World, set 1)
 // World version with no copyright notice
 
 static struct BurnRomInfo turbofrcRomDesc[] = {
@@ -2870,10 +2872,52 @@ STD_ROM_FN(turbofrc)
 
 struct BurnDriver BurnDrvTurbofrc = {
 	"turbofrc", NULL, NULL, NULL, "1991",
-	"Turbo Force (World)\0", NULL, "Video System Co.", "Video System",
+	"Turbo Force (World, set 1)\0", NULL, "Video System Co.", "Video System",
 	NULL, NULL, NULL, NULL,
 	BDF_GAME_WORKING | BDF_ORIENTATION_VERTICAL, 3, HARDWARE_MISC_POST90S, GBF_VERSHOOT, 0,
 	NULL, turbofrcRomInfo, turbofrcRomName, NULL, NULL, turbofrcInputInfo, turbofrcDIPInfo,
+	turbofrcInit,DrvExit,DrvFrame,turbofrcDraw,DrvScan,&DrvRecalc,0x400,
+	240,352,3,4
+};
+
+
+// Turbo Force (World, set 2)
+// World version with no copyright notice
+
+static struct BurnRomInfo turbofrcoRomDesc[] = {
+	{ "3v2.subpcb.u2",	0x040000, 0x721300ee, BRF_ESS | BRF_PRG }, // 68000 code swapped
+	{ "3v1.subpcb.u1",  0x040000, 0x71b6431b, BRF_ESS | BRF_PRG },
+	{ "3v3.u14",    	0x040000, 0x63f50557, BRF_ESS | BRF_PRG },
+	
+	{ "lh534ggs.u94",  	0x080000, 0xbaa53978, BRF_GRA },		   // gfx1
+	{ "7.u95",  	  	0x020000, 0x71a6c573, BRF_GRA },
+	
+	{ "lh534ggy.u105", 	0x080000, 0x4de4e59e, BRF_GRA },		   // gfx2
+	{ "8.u106", 	  	0x020000, 0xc6479eb5, BRF_GRA },
+	
+	{ "lh534gh2.u116", 	0x080000, 0xdf210f3b, BRF_GRA },		   // gfx3
+	{ "5.u118", 	  	0x040000, 0xf61d1d79, BRF_GRA },
+	{ "lh534gh1.u117", 	0x080000, 0xf70812fd, BRF_GRA },
+	{ "4.u119", 	  	0x040000, 0x474ea716, BRF_GRA },
+
+	{ "lh532a52.u134", 	0x040000, 0x3c725a48, BRF_GRA },		   // gfx4
+	{ "lh532a51.u135", 	0x040000, 0x95c63559, BRF_GRA },
+
+	{ "6.u166", 	  	0x020000, 0x2ca14a65, BRF_ESS | BRF_PRG }, // Sound CPU
+	
+	{ "lh532h74.u180", 	0x040000, 0xa3d43254, BRF_SND },		   // samples
+	{ "lh538o7j.u179", 	0x100000, 0x60ca0333, BRF_SND },	
+};
+
+STD_ROM_PICK(turbofrco)
+STD_ROM_FN(turbofrco)
+
+struct BurnDriver BurnDrvTurbofrco = {
+	"turbofrco", "turbofrc", NULL, NULL, "1991",
+	"Turbo Force (World, set 2)\0", NULL, "Video System Co.", "Video System",
+	NULL, NULL, NULL, NULL,
+	BDF_GAME_WORKING | BDF_CLONE | BDF_ORIENTATION_VERTICAL, 3, HARDWARE_MISC_POST90S, GBF_VERSHOOT, 0,
+	NULL, turbofrcoRomInfo, turbofrcoRomName, NULL, NULL, turbofrcInputInfo, turbofrcDIPInfo,
 	turbofrcInit,DrvExit,DrvFrame,turbofrcDraw,DrvScan,&DrvRecalc,0x400,
 	240,352,3,4
 };

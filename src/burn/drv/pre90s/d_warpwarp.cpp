213c4487f8c5ef2f0eb0e49a541dcca4ebd52f6e
jackchatelet <jackchatelet@f2e10478-e7fb-40a4-b0e2-024e28a34407>
2018-02-03 09:48:08 +0000

    d_warpwarp.cpp: added Gee Bee (UK) [Andrew Welburn, The Dumping Union]

--- src/burn/drv/pre90s/d_warpwarp.cpp
+++ src/burn/drv/pre90s/d_warpwarp.cpp
@@ -943,7 +943,7 @@ static INT32 DrvInit()
 
 	if (!strncmp(BurnDrvGetTextA(DRV_NAME), "geebee", 6)) {
 		bprintf(0, _T("geebee mode"));
-		if (!strncmp(BurnDrvGetTextA(DRV_NAME), "geebeeb", 7)) { // US version
+		if (!strncmp(BurnDrvGetTextA(DRV_NAME), "geebeea", 7) || !strncmp(BurnDrvGetTextA(DRV_NAME), "geebeeb", 7)) { // US version
 			if (BurnLoadRom(DrvZ80ROM + 0x0000, 0, 1)) return 1;
 			if (BurnLoadRom(DrvZ80ROM + 0x0400, 1, 1)) return 1;
 			if (BurnLoadRom(DrvZ80ROM + 0x0800, 2, 1)) return 1;
@@ -1328,6 +1328,30 @@ struct BurnDriver BurnDrvGeebeeg = {
 	NULL, NULL, NULL, NULL,
 	BDF_GAME_WORKING | BDF_CLONE | BDF_ORIENTATION_VERTICAL | BDF_ORIENTATION_FLIPPED, 2, HARDWARE_MISC_PRE90S, GBF_BREAKOUT, 0,
 	NULL, geebeegRomInfo, geebeegRomName, NULL, NULL, GeebeeInputInfo, GeebeeDIPInfo,
+	GeebeeInit, DrvExit, DrvFrame, DrvDraw, DrvScan, &DrvRecalc, 0x300,
+	224, 272, 3, 4
+};
+
+// Gee Bee (UK)
+
+static struct BurnRomInfo geebeeaRomDesc[] = {
+	{ "1.1m",	0x0400, 0x23252fc7, 1 | BRF_PRG | BRF_ESS }, //  0 maincpu
+	{ "2.1p",	0x0400, 0x0bc4d4ca, 1 | BRF_PRG | BRF_ESS }, //  1
+	{ "3.1s",	0x0400, 0x7899b4c1, 1 | BRF_PRG | BRF_ESS }, //  2
+	{ "4.1t",	0x0400, 0x0b6e6fcb, 1 | BRF_PRG | BRF_ESS }, //  3
+
+	{ "a_136",	0x0400, 0xbd01437d, 2 | BRF_GRA },           //  4 gfx1
+};
+
+STD_ROM_PICK(geebeea)
+STD_ROM_FN(geebeea)
+
+struct BurnDriver BurnDrvGeebeea = {
+	"geebeea", "geebee", NULL, NULL, "1978",
+	"Gee Bee (UK)\0", NULL, "Namco (Alca license)", "Miscellaneous",
+	NULL, NULL, NULL, NULL,
+	BDF_GAME_WORKING | BDF_CLONE | BDF_ORIENTATION_VERTICAL | BDF_ORIENTATION_FLIPPED, 2, HARDWARE_MISC_PRE90S, GBF_BREAKOUT, 0,
+	NULL, geebeeaRomInfo, geebeeaRomName, NULL, NULL, GeebeeInputInfo, GeebeeDIPInfo,
 	GeebeeInit, DrvExit, DrvFrame, DrvDraw, DrvScan, &DrvRecalc, 0x300,
 	224, 272, 3, 4
 };

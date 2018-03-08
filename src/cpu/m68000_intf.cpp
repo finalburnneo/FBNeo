d2a09b27790f039c83349b534d22490cbf013598
dinkc64 <12570148+dinkc64@users.noreply.github.com>
2018-03-08 03:20:51 +0000

    m68k: scan nSekCycles

--- src/cpu/m68000_intf.cpp
+++ src/cpu/m68000_intf.cpp
@@ -2109,7 +2109,8 @@ INT32 SekScan(INT32 nAction)
 		szName[9] = '0' + i;
 
 		SCAN_VAR(nSekCPUType[i]);
-		SCAN_VAR(nSekIRQPending[i]); // fix for Gradius 2 s.states -dink feb.3.2015
+		SCAN_VAR(nSekIRQPending[i]);
+		SCAN_VAR(nSekCycles[i]);
 
 #if defined EMU_A68K && defined EMU_M68K
 		// Switch to another core if needed

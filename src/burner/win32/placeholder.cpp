8ceeac058b121283d649dc07df2936bd11be92b1
Barry Harris <44396066+barry65536@users.noreply.github.com>
2018-05-18 10:27:09 +0000

    need this

---
+++ src/burner/win32/placeholder.cpp
@@ -0,0 +1,43 @@
+#include "burner.h"
+
+static void MakeOfn()
+{
+	memset(&ofn, 0, sizeof(ofn));
+	ofn.lStructSize = sizeof(ofn);
+	ofn.hwndOwner = hScrnWnd;
+	ofn.lpstrFilter = _T("FB Alpha skin files (*.bmp,*.png)\0*.bmp;*.png\0\0)");
+	ofn.lpstrFile = szChoice;
+	ofn.nMaxFile = sizeof(szChoice) / sizeof(TCHAR);
+	ofn.lpstrInitialDir = _T("");
+	ofn.Flags = OFN_NOCHANGEDIR | OFN_HIDEREADONLY;
+	ofn.lpstrDefExt = _T("png");
+	return;
+}
+
+int SelectPlaceHolder()
+{
+	int nRet;
+	int bOldPause;
+
+	MakeOfn();
+	ofn.lpstrTitle = FBALoadStringEx(hAppInst, IDS_PLACEHOLDER_LOAD, true);
+
+	bOldPause = bRunPause;
+	bRunPause = 1;
+	nRet = GetOpenFileName(&ofn);
+	bRunPause = bOldPause;
+
+	if (nRet == 0) {		// Error
+		return 1;
+	}
+
+	szPlaceHolder[0] = _T('\0');
+	memcpy(szPlaceHolder, szChoice, sizeof(szChoice));
+	
+	return nRet;
+}
+
+void ResetPlaceHolder()
+{
+	szPlaceHolder[0] = _T('\0');
+}

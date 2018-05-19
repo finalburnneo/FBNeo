1836dd48245a44760b4e45f7e8bc7bcdabecfb4c
jackchatelet <jackchatelet@f2e10478-e7fb-40a4-b0e2-024e28a34407>
2018-05-19 10:24:18 +0000

    d_bionicc.cpp: corrected MCU label for the original sets [Guru]

--- src/burn/drv/pre90s/d_bionicc.cpp
+++ src/burn/drv/pre90s/d_bionicc.cpp
@@ -804,7 +804,7 @@ static struct BurnRomInfo bioniccRomDesc[] = {
 
 	{ "63s141.18f",		0x00100, 0xb58d0023, 0 | BRF_OPT },				// 24 Priority (not used)
 	
-	{ "d8751h.bin",     0x01000, 0x3ed7f0be, 0 | BRF_OPT },
+	{ "ts.2f",     		0x01000, 0x3ed7f0be, 0 | BRF_OPT },
 };
 
 STD_ROM_PICK(bionicc)
@@ -856,7 +856,7 @@ static struct BurnRomInfo bionicc1RomDesc[] = {
 
 	{ "63s141.18f",		0x00100, 0xb58d0023, 0 | BRF_OPT },				// 24 Priority (not used)
 	
-	{ "d8751h.bin",     0x01000, 0x3ed7f0be, 0 | BRF_OPT },
+	{ "ts.2f",     		0x01000, 0x3ed7f0be, 0 | BRF_OPT },
 };
 
 STD_ROM_PICK(bionicc1)
@@ -908,7 +908,7 @@ static struct BurnRomInfo bionicc2RomDesc[] = {
 
 	{ "63s141.18f",		0x00100, 0xb58d0023, 0 | BRF_OPT },				// 24 Priority (not used)
 	
-	{ "d8751h.bin",     0x01000, 0x3ed7f0be, 0 | BRF_OPT },
+	{ "ts.2f",     		0x01000, 0x3ed7f0be, 0 | BRF_OPT },
 };
 
 STD_ROM_PICK(bionicc2)
@@ -961,7 +961,7 @@ static struct BurnRomInfo topsecrtRomDesc[] = {
 	{ "63s141.18f",		0x00100, 0xb58d0023, 0 | BRF_OPT },				// 24 Priority (not used)
 	
 //	{ "c8751h-88",      0x01000, 0x00000000, 0 | BRF_OPT | BRF_NODUMP },
-	{ "d8751h.bin",     0x01000, 0x3ed7f0be, 0 | BRF_OPT },
+	{ "ts.2f",     		0x01000, 0x3ed7f0be, 0 | BRF_OPT },
 };
 
 STD_ROM_PICK(topsecrt)

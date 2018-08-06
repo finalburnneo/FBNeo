09c074e10200c8ae5fb21978778c24fbc46e9b47
dinkc64 <12570148+dinkc64@users.noreply.github.com>
2018-08-06 03:43:28 +0000

    add atari mathbox device

---
+++ src/burn/devices/mathbox.h
@@ -0,0 +1,6 @@
+void mathbox_reset();
+void mathbox_scan(INT32 nAction, INT32 *);
+void mathbox_go_write(UINT8 offset, UINT8 data);
+UINT8 mathbox_status_read();
+UINT8 mathbox_lo_read();
+UINT8 mathbox_hi_read();

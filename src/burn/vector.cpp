23bcd6212b180bacf3fb9b27d112ab557bd987c0
iq_132 <1191709+iq132@users.noreply.github.com>
2018-08-09 01:03:23 +0000

    Fix up vector drawing

--- src/burn/vector.cpp
+++ src/burn/vector.cpp
@@ -14,6 +14,8 @@ struct vector_line {
 static struct vector_line *vector_table;
 struct vector_line *vector_ptr; // pointer
 static INT32 vector_cnt;
+static UINT32 *pBitmap = NULL;
+static UINT32 *pPalette = NULL;
 
 void vector_add_point(INT32 x, INT32 y, INT32 color, INT32 intensity)
 {
@@ -31,6 +33,8 @@ void vector_add_point(INT32 x, INT32 y, INT32 color, INT32 intensity)
 static void lineSimple(INT32 x0, INT32 y0, INT32 x1, INT32 y1, INT32 color, INT32 intensity)
 {
 	color = color * 256 + intensity;
+	UINT32 p = pPalette[color];
+	if (p == 0) return; // safe to assume we can't draw black??
 
 	// http://rosettacode.org/wiki/Bitmap/Bresenham%27s_line_algorithm
 
@@ -43,7 +47,24 @@ static void lineSimple(INT32 x0, INT32 y0, INT32 x1, INT32 y1, INT32 color, INT3
 	while (1)
 	{
 		if (x0 >= 0 && x0 < nScreenWidth && y0 >= 0 && y0 < nScreenHeight) {
-			pTransDraw[y0 * nScreenWidth + x0] = color;
+			INT32 coords = y0 * nScreenWidth + x0;
+	
+			UINT32 d = pBitmap[coords];
+
+			if (d) { // is this actually how it works?
+				INT32 r = ((d >> 16) & 0xff) + ((p >> 16) & 0xff);
+				INT32 g = ((d >>  8) & 0xff) + ((p >>  8) & 0xff);
+				INT32 b = (d & 0xff) + (p & 0xff);
+				if (b > 0xff) b = 0xff; // clamp
+				if (r > 0xff) r = 0xff; // clamp
+				if (g > 0xff) g = 0xff; // clamp
+	
+				pBitmap[coords] = (r << 16) | (g << 8) | b;
+			}
+			else
+			{
+				pBitmap[y0 * nScreenWidth + x0] = p;
+			}
 		}
 
 		if (x0 == x1 && y0 == y1) break;
@@ -61,7 +82,8 @@ void draw_vector(UINT32 *palette)
 
 	INT32 prev_x = 0, prev_y = 0;
 
-	BurnTransferClear();
+	memset (pBitmap, 0, nScreenWidth * nScreenHeight * sizeof(INT32));
+	pPalette = palette;
 
 	for (INT32 i = 0; i < vector_cnt && i < TABLE_SIZE; i++, ptr++)
 	{
@@ -78,7 +100,20 @@ void draw_vector(UINT32 *palette)
 		prev_y = curr_y;
 	}
 
-	BurnTransferCopy(palette);
+	// copy to the screen, only draw pixels that aren't black
+	// should be safe for any bit depth with putpix
+	{
+		memset (pBurnDraw, 0, nScreenWidth * nScreenHeight * nBurnBpp);
+
+		for (INT32 i = 0; i < nScreenWidth * nScreenHeight; i++)
+		{
+			UINT32 p = pBitmap[i];
+			
+			if (p) {
+				PutPix(pBurnDraw + i * nBurnBpp, BurnHighCol((p >> 16) & 0xff, (p >> 8) & 0xff, p & 0xff, 0));
+			}
+		}
+	}
 }
 
 void vector_reset()
@@ -91,6 +126,8 @@ void vector_reset()
 void vector_init()
 {
 	GenericTilesInit();
+	
+	pBitmap = (UINT32*)BurnMalloc(nScreenWidth * nScreenHeight * sizeof(INT32));
 
 	vector_table = (struct vector_line*)BurnMalloc(TABLE_SIZE * sizeof(vector_line));
 
@@ -102,6 +139,11 @@ void vector_init()
 void vector_exit()
 {
 	GenericTilesExit();
+	
+	if (pBitmap) {
+		BurnFree (pBitmap);
+	}
+	pPalette = NULL;
 
 	BurnFree (vector_table);
 	vector_ptr = NULL;

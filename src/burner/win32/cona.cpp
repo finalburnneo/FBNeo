d5965e87fba381f3bac4b1bbbb2cffc82fe05f3f
Ryvius86 <ryvius86@hotmail.com>
2019-05-11 06:29:31 +0100

    load game menu - ui, filters & text revision

--- src/burner/win32/cona.cpp
+++ src/burner/win32/cona.cpp
@@ -195,6 +195,8 @@ int ConfigAppLoad()
 		VAR(nLoadMenuGenreFilter);
 		VAR(nLoadMenuFavoritesFilter);
 		VAR(nLoadMenuFamilyFilter);
+		VAR(nLoadMenuCapcomGrpFilter);
+		VAR(nLoadMenuSegaGrpFilter);
 
 		STR(szAppRomPaths[0]);
 		STR(szAppRomPaths[1]);
@@ -574,6 +576,12 @@ int ConfigAppSave()
 	
 	_ftprintf(h, _T("\n// Load game dialog family filter options\n"));
 	VAR(nLoadMenuFamilyFilter);
+	
+	_ftprintf(h, _T("\n// Load game dialog capcom filter options\n"));
+	VAR(nLoadMenuCapcomGrpFilter);
+	
+	_ftprintf(h, _T("\n// Load game dialog sega filter options\n"));	
+	VAR(nLoadMenuSegaGrpFilter);
 
 	_ftprintf(h, _T("\n// The paths to search for rom zips (include trailing backslash)\n"));
 	STR(szAppRomPaths[0]);

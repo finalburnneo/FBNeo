16f11e16dcd60c304a311447fa9062ed24c9c8e8
dinkc64 <terra79pin@yahoo.com>
2019-08-27 08:18:57 -0400

    remove addfamp2 from megadrive - it's completely broken

--- src/burn/drv/megadrive/d_megadrive.cpp
+++ src/burn/drv/megadrive/d_megadrive.cpp
@@ -2237,24 +2237,6 @@ struct BurnDriver BurnDrvmd_addfamp1 = {
 	&bMegadriveRecalcPalette, 0x100, 320, 224, 4, 3
 };
 
-// The Addams Family (USA, Prototype, Alt)
-static struct BurnRomInfo md_addfamp2RomDesc[] = {
-	{ "addams family, the (usa) (beta) (alt).bin", 0x100000, 0x3a173e1f, BRF_PRG | SEGA_MD_ROM_LOAD16_WORD_SWAP | SEGA_MD_ROM_OFFS_000000  },
-};
-
-STD_ROM_PICK(md_addfamp2)
-STD_ROM_FN(md_addfamp2)
-
-struct BurnDriver BurnDrvmd_addfamp2 = {
-	"md_addfamp2", "md_addfam", NULL, NULL, "1994",
-	"The Addams Family (USA, Prototype, Alt)\0", NULL, "Flying Edge", "Sega Megadrive",
-	NULL, NULL, NULL, NULL,
-	BDF_GAME_WORKING | BDF_16BIT_ONLY | BDF_CLONE, 2, HARDWARE_SEGA_MEGADRIVE, GBF_MISC, 0,
-	MegadriveGetZipName, md_addfamp2RomInfo, md_addfamp2RomName, NULL, NULL, NULL, NULL, MegadriveInputInfo, MegadriveDIPInfo,
-	MegadriveInit, MegadriveExit, MegadriveFrame, MegadriveDraw, MegadriveScan,
-	&bMegadriveRecalcPalette, 0x100, 320, 224, 4, 3
-};
-
 // Advanced Daisenryaku - Deutsch Dengeki Sakusen (Jpn, Rev. A)
 static struct BurnRomInfo md_advdaisnRomDesc[] = {
 	{ "mpr-13842a.bin", 0x100000, 0xe0d5e18a, BRF_PRG | SEGA_MD_ROM_LOAD16_WORD_SWAP | SEGA_MD_ROM_OFFS_000000  },

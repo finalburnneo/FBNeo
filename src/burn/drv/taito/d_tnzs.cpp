748d2e8b16051ba97bd416b5dc30f41e4f6a11f5
dinkc64 <terra79pin@yahoo.com>
2019-09-26 12:27:46 -0400

    arkanoid 2 proper analog hookup

--- src/burn/drv/taito/d_tnzs.cpp
+++ src/burn/drv/taito/d_tnzs.cpp
@@ -3,6 +3,7 @@
 
 #include "tiles_generic.h"
 #include "z80_intf.h"
+#include "burn_gun.h"
 #include "burn_ym2203.h"
 #include "burn_ym2151.h"
 #include "tnzs_prot.h"
@@ -56,8 +57,7 @@ static UINT8 DrvDips[2];
 static UINT8 DrvInputs[3];
 static UINT8 DrvReset;
 
-static UINT16 DrvAxis[2];
-static INT32 nAnalogAxis[2] = { 0, 0 };
+static INT16 DrvAxis[2];
 
 static INT32 nExtraCycles[3];
 
@@ -793,16 +793,15 @@ static UINT8 __fastcall tnzs_cpu1_read(UINT16 address)
 			return DrvDips[1];
 
 		case 0xf000:
-			return (~nAnalogAxis[0] >> 12) & 0xff;
-
 		case 0xf001:
-			return (~nAnalogAxis[0] >> 20) & 0x0f;
-
 		case 0xf002:
-			return (~nAnalogAxis[1] >> 12) & 0xff;
-
-		case 0xf003:
-			return (~nAnalogAxis[1] >> 20) & 0x0f;
+		case 0xf003: {
+			UINT32 t = BurnTrackballReadWord(0, (address & 3) >> 1) & 0xfff;
+			if (address & 1)
+				return (t >> 8) & 0xff;
+			else
+				return t & 0xff;
+		}
 	}
 
 	return 0;
@@ -1358,6 +1357,8 @@ static INT32 Type1Init(INT32 mcutype)
 	DACInit(0, 0, 1, ZetTotalCycles, 6000000); // kabukiz
 	DACSetRoute(0, 0.50, BURN_SND_ROUTE_BOTH);
 
+	BurnTrackballInit(2);
+
 	GenericTilesInit();
 
 	DrvDoReset();
@@ -1471,6 +1472,8 @@ static INT32 Type2Init()
 
 	GenericTilesInit();
 
+	BurnTrackballInit(2); // not on this hw
+
 	DrvDoReset();
 
 	return 0;
@@ -1487,6 +1490,8 @@ static INT32 DrvExit()
 	DACExit();
 
 	BurnFree (AllMem);
+
+	BurnTrackballExit();
 
 	if (tnzs_mcu_type() == MCU_NONE_KAGEKI) {
 		kageki_sample_exit();
@@ -1741,8 +1746,9 @@ static void assemble_inputs()
 		DrvInputs[2] ^= DrvJoy3[i] << i;
 	}
 
-	nAnalogAxis[0] -= DrvAxis[0] << 7;
-	nAnalogAxis[1] -= DrvAxis[1] << 7;
+	BurnTrackballConfig(0, AXIS_NORMAL, AXIS_NORMAL);
+	BurnTrackballFrame(0, DrvAxis[0], DrvAxis[1], 0x6, 0x2f);
+	BurnTrackballUpdate(0);
 }
 
 static INT32 DrvFrame()
@@ -1763,7 +1769,7 @@ static INT32 DrvFrame()
 	for (INT32 i = 0; i < nInterleave; i++) {
 		// Run Z80 #0
 		ZetOpen(0);
-		nCyclesDone[0] += ZetRun(((i + 1) * nCyclesTotal[0] / nInterleave) - nCyclesDone[0]);
+		CPU_RUN(0, Zet);
 		if (i == 240) {
 			tnzs_mcu_interrupt();
 			ZetSetIRQLine(0, CPU_IRQSTATUS_HOLD);
@@ -1776,7 +1782,7 @@ static INT32 DrvFrame()
 			if (system_type == 1 && tnzs_mcu_type() != MCU_NONE_JPOPNICS)
 				BurnTimerUpdate((i + 1) * (nCyclesTotal[1] / nInterleave));
 			else
-				nCyclesDone[1] += ZetRun(((i + 1) * nCyclesTotal[1] / nInterleave) - nCyclesDone[1]);
+				CPU_RUN(1, Zet);
 		}
 		if (i == 240)
 			ZetSetIRQLine(0, CPU_IRQSTATUS_HOLD);
@@ -1871,12 +1877,12 @@ static INT32 DrvScan(INT32 nAction,INT32 *pnMin)
 
 		DACScan(nAction, pnMin);
 
+		BurnTrackballScan();
+
 		tnzs_mcu_scan();
 
 		SCAN_VAR(tnzs_banks);
 		SCAN_VAR(cpu1_reset);
-
-		SCAN_VAR(nAnalogAxis);
 
 		SCAN_VAR(kageki_csport_sel);
 		SCAN_VAR(kageki_sample_pos);

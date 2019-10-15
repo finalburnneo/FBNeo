37a6c20a2d36a765f71fd1a052ba5e63f8cb7d6d
Akop Karapetyan <dev@akop.org>
2019-10-15 11:35:35 -0700

    Stubbed build

---
+++ projectfiles/xcode/Emulator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FinalBurnNeo
+//
+//  Created by Akop Karapetyan on 10/15/19.
+//  Copyright © 2019 Akop Karapetyan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ src/intf/video/macos/vid_macos.cpp
@@ -0,0 +1,11 @@
+#include "burner.h"
+
+struct VidOut VidOutMacOS = {
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    _T("MacOS Video"),
+};

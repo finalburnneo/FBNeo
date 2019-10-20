501d815a3193c516958e80a8cddefd4ac8cb8a2f
Akop Karapetyan <dev@akop.org>
2019-10-19 23:37:38 -0700

    Added video rendering

--- projectfiles/xcode/Emulator/AppDelegate.m
+++ projectfiles/xcode/Emulator/AppDelegate.m
@@ -1,4 +1,4 @@
-//(
+//
 //  AppDelegate.m
 //  FinalBurnNeo
 //
@@ -9,21 +9,38 @@
 #import "AppDelegate.h"
 
 #import "FBMainThread.h"
+#import "FBVideo.h"
 
 @interface AppDelegate ()
 
 @property (weak) IBOutlet NSWindow *window;
 
+- (NSSize) gameScreenSize;
+
 @end
+
+static AppDelegate *sharedInstance = nil;
 
 @implementation AppDelegate
 {
     FBMainThread *main;
+    BOOL _cursorVisible;
+}
+
+- (void)dealloc
+{
+    screen.delegate = nil;
 }
 
 - (void) awakeFromNib
 {
+    sharedInstance = self;
+    _video = [FBVideo new];
     main = [FBMainThread new];
+
+    _cursorVisible = YES;
+    screen.delegate = self;
+    _video.delegate = screen;
 }
 
 - (void) applicationDidFinishLaunching:(NSNotification *)aNotification {
@@ -42,6 +59,47 @@
     [main cancel];
 }
 
+- (NSSize) windowWillResize:(NSWindow *) sender
+                     toSize:(NSSize) frameSize
+{
+    NSSize screenSize = [self gameScreenSize];
+    NSRect windowFrame = [[self window] frame];
+    NSView *contentView = [[self window] contentView];
+    NSRect viewRect = [contentView convertRect:[contentView bounds]
+                                        toView:nil];
+    NSRect contentRect = [[self window] contentRectForFrameRect:windowFrame];
+
+    CGFloat screenRatio = screenSize.width / screenSize.height;
+
+    float marginY = viewRect.origin.y + windowFrame.size.height - contentRect.size.height;
+    float marginX = contentRect.size.width - viewRect.size.width;
+
+    // Clamp the minimum height
+    if ((frameSize.height - marginY) < screenSize.height) {
+        frameSize.height = screenSize.height + marginY;
+    }
+
+    // Set the screen width as a percentage of the screen height
+    frameSize.width = (frameSize.height - marginY) * screenRatio + marginX;
+
+    return frameSize;
+}
+
+- (void) windowDidResize:(NSNotification *) notification
+{
+    NSSize screenSize = [self gameScreenSize];
+    if (screenSize.width != 0 && screenSize.height != 0) {
+        NSRect windowFrame = [[self window] frame];
+        NSRect contentRect = [[self window] contentRectForFrameRect:windowFrame];
+
+        NSString *screenSizeString = NSStringFromSize(screenSize);
+        NSString *actualSizeString = NSStringFromSize(contentRect.size);
+
+        [[NSUserDefaults standardUserDefaults] setObject:actualSizeString
+                                                  forKey:[@"preferredSize-" stringByAppendingString:screenSizeString]];
+    }
+}
+
 - (BOOL) applicationShouldTerminateAfterLastWindowClosed:(NSApplication *) sender
 {
     return YES;
@@ -52,6 +110,40 @@
 {
     main.fileToOpen = filename;
     return YES;
+}
+
++ (AppDelegate *) sharedInstance
+{
+    return sharedInstance;
+}
+
+#pragma mark - FBScreenViewDelegate
+
+- (void) mouseDidIdle
+{
+    if (_cursorVisible) {
+        _cursorVisible = NO;
+        [NSCursor hide];
+    }
+}
+
+- (void) mouseStateDidChange
+{
+    if (!_cursorVisible) {
+        _cursorVisible = YES;
+        [NSCursor unhide];
+    }
+}
+
+#pragma mark - Private
+
+extern int BurnDrvGetVisibleSize(int* pnWidth, int* pnHeight);
+
+- (NSSize) gameScreenSize
+{
+    int w, h;
+    BurnDrvGetVisibleSize(&w, &h);
+    return NSMakeSize(w, h);
 }
 
 @end
---
+++ src/intf/video/macos/FBVideo.mm
@@ -0,0 +1,186 @@
+// Portions from FinalBurn X: Port of FinalBurn to OS X
+//   https://github.com/0xe1f/FinalBurn-X
+//
+// Copyright (C) Akop Karapetyan
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   http://www.apache.org/licenses/LICENSE-2.0
+
+#import "FBVideo.h"
+
+#import "AppDelegate.h"
+
+#include "burner.h"
+
+@implementation FBVideo
+
+#pragma mark - Init and dealloc
+
+- (instancetype) init
+{
+    if (self = [super init]) {
+    }
+
+    return self;
+}
+
+- (void) dealloc
+{
+	_delegate = nil;
+}
+
+#pragma mark - Intermediaries
+
+- (BOOL) renderToSurface:(unsigned char *) buffer
+{
+    id<FBVideoDelegate> vd = [self delegate];
+    if ([vd respondsToSelector:@selector(renderFrame:)])
+        [vd renderFrame:buffer];
+
+    return YES;
+}
+
+- (void) notifyTextureReadyOfWidth:(int) texWidth
+                            height:(int) texHeight
+                         isRotated:(BOOL) isRotated
+                     bytesPerPixel:(int) bpp
+                        screenSize:(NSSize) size
+{
+    dispatch_async(dispatch_get_main_queue(), ^{
+        id<FBVideoDelegate> vd = [self delegate];
+        if ([vd respondsToSelector:@selector(initTextureOfWidth:height:isRotated:bytesPerPixel:)])
+            [vd initTextureOfWidth:texWidth
+                    height:texHeight
+                 isRotated:isRotated
+             bytesPerPixel:bpp];
+
+        if ([vd respondsToSelector:@selector(screenSizeDidChange:)])
+            [vd screenSizeDidChange:size];
+    });
+}
+
+@end
+
+#pragma mark - FinalBurn callbacks
+
+static unsigned char *screenBuffer = NULL;
+static int bufferWidth = 0;
+static int bufferHeight = 0;
+static int bufferBytesPerPixel = 0;
+
+static int MacOSVideoInit()
+{
+    int gameWidth;
+    int gameHeight;
+    int rotationMode = 0;
+
+    BurnDrvGetVisibleSize(&gameWidth, &gameHeight);
+
+    if (BurnDrvGetFlags() & BDF_ORIENTATION_VERTICAL) {
+        rotationMode |= 1;
+    }
+
+    if (BurnDrvGetFlags() & BDF_ORIENTATION_FLIPPED) {
+        rotationMode ^= 2;
+    }
+
+    nVidImageWidth = gameWidth;
+    nVidImageHeight = gameHeight;
+    nVidImageDepth = 24;
+    nVidImageBPP = 3;
+    if (!rotationMode) {
+        nVidImagePitch = nVidImageWidth * nVidImageBPP;
+    } else {
+        nVidImagePitch = nVidImageHeight * nVidImageBPP;
+    }
+
+    SetBurnHighCol(nVidImageDepth);
+
+    bufferBytesPerPixel = nVidImageBPP;
+    bufferWidth = gameWidth;
+    bufferHeight = gameHeight;
+
+    int bufSize = bufferWidth * bufferHeight * nVidImageBPP;
+    free(screenBuffer);
+    screenBuffer = (unsigned char *) malloc(bufSize);
+
+    if (screenBuffer == NULL)
+        return 1;
+
+    nBurnBpp = nVidImageBPP;
+    nBurnPitch = nVidImagePitch;
+    pVidImage = screenBuffer;
+
+    memset(screenBuffer, 0, bufSize);
+
+    int textureWidth;
+    int textureHeight;
+    BOOL isRotated = rotationMode & 1;
+
+    if (!isRotated) {
+        textureWidth = bufferWidth;
+        textureHeight = bufferHeight;
+    } else {
+        textureWidth = bufferHeight;
+        textureHeight = bufferWidth;
+    }
+
+    NSSize screenSize = NSMakeSize((CGFloat)bufferWidth,
+                                   (CGFloat)bufferHeight);
+
+    [AppDelegate.sharedInstance.video notifyTextureReadyOfWidth:textureWidth
+                                                         height:textureHeight
+                                                      isRotated:isRotated
+                                                  bytesPerPixel:bufferBytesPerPixel
+                                                     screenSize:screenSize];
+
+    return 0;
+}
+
+static int MacOSVideoExit()
+{
+    free(screenBuffer);
+    screenBuffer = NULL;
+
+    return 0;
+}
+
+static int MacOSVideoFrame(bool redraw)
+{
+    if (pVidImage == NULL)
+        return NO;
+
+    if (redraw) {
+        if (BurnDrvRedraw()) {
+            BurnDrvFrame();
+        }
+    } else {
+        BurnDrvFrame();
+    }
+
+    return 0;
+}
+
+static int MacOSVideoPaint(int validate)
+{
+    return [AppDelegate.sharedInstance.video renderToSurface:screenBuffer] ? 0 : 1;
+}
+
+static int MacOSVideoScale(RECT*, int, int)
+{
+	return 0;
+}
+
+static int MacOSVideoGetSettings(InterfaceInfo *info)
+{
+	return 0;
+}
+
+struct VidOut VidOutMacOS = {
+    MacOSVideoInit,
+    MacOSVideoExit,
+    MacOSVideoFrame,
+    MacOSVideoPaint,
+    MacOSVideoScale,
+    MacOSVideoGetSettings,
+    _T("MacOS Video"),
+};

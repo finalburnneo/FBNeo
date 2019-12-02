8612420107cd63bbc55fad17da1756c4bc944420
Akop Karapetyan <2538706+0xe1f@users.noreply.github.com>
2019-12-01 21:04:29 -0800

    Launcher now the initial window

---
+++ projectfiles/xcode/Emulator/Views/FBRomSetStatusAsNSImage.h
@@ -0,0 +1,13 @@
+//
+//  FBRomSetStatusAsNSImage.h
+//  FinalBurnNeo
+//
+//  Created by Akop Karapetyan on 12/01/19.
+//  Copyright © 2019 Akop Karapetyan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FBRomSetStatusAsNSImage : NSValueTransformer
+
+@end

3e6b56b9ca305b684b8a4832271aad88d8158c70
tmaul <notkevarmstrong@gmail.com>
2019-12-21 22:46:12 +0000

    fix crash with PCE by limiting players to 4

--- src/burner/gami.cpp
+++ src/burner/gami.cpp
@@ -356,7 +356,7 @@ static void GameInpInitMacros()
 			pgi->nType = BIT_DIGITAL;
 			pgi->Macro.nMode = 0;
 
-			sprintf(pgi->Macro.szName, "P%i 3� Punch", nPlayer + 1);
+			sprintf(pgi->Macro.szName, "P%i 3x Punch", nPlayer + 1);
 			for (INT32 j = 0; j < 3; j++) {
 				BurnDrvGetInputInfo(&bii, nPunchInputs[nPlayer][j]);
 				pgi->Macro.pVal[j] = bii.pVal;
@@ -372,7 +372,7 @@ static void GameInpInitMacros()
 			pgi->nType = BIT_DIGITAL;
 			pgi->Macro.nMode = 0;
 
-			sprintf(pgi->Macro.szName, "P%i 3� Kick", nPlayer + 1);
+			sprintf(pgi->Macro.szName, "P%i 3x Kick", nPlayer + 1);
 			for (INT32 j = 0; j < 3; j++) {
 				BurnDrvGetInputInfo(&bii, nKickInputs[nPlayer][j]);
 				pgi->Macro.pVal[j] = bii.pVal;

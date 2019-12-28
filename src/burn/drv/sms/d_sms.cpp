6d11e6ad1f6b588c253ac56008ba702c2299b0ea
dinkc64 <terra79pin@yahoo.com>
2019-12-27 23:07:21 -0500

    add Voyage - A Sorceress' Vacation homebrew (AKIMW hack) to SMS, get rom patch etc https://www.smspower.org/forums/17753-VoyageASorceressVacationAKiMWTotalConversionFormerlyKnowAsMaKaiQuest

--- src/burn/drv/sms/d_sms.cpp
+++ src/burn/drv/sms/d_sms.cpp
@@ -27683,3 +27683,21 @@ struct BurnDriver BurnDrvsms_novdmvdp = {
 	256, 192, 4, 3
 };
 
+// Voyage - A Sorceress Vacation v1.0 (build 684)
+
+static struct BurnRomInfo sms_voyageRomDesc[] = {
+	{ "Voyage - A Sorceress Vacation v1.0 (build 684).sms",	0x80000, 0x80afc57a, BRF_PRG | BRF_ESS },
+};
+
+STD_ROM_PICK(sms_voyage)
+STD_ROM_FN(sms_voyage)
+
+struct BurnDriver BurnDrvsms_voyage = {
+	"sms_voyage", NULL, NULL, NULL, "2019",
+	"Voyage - A Sorceress Vacation (v1.0 build 684)\0", NULL, "SNAGS", "Sega Master System",
+	NULL, NULL, NULL, NULL,
+	BDF_GAME_WORKING, 2, HARDWARE_SEGA_MASTER_SYSTEM, GBF_MISC, 0,
+	SMSGetZipName, sms_voyageRomInfo, sms_voyageRomName, NULL, NULL, NULL, NULL, SMSInputInfo, SMSDIPInfo,
+	SMSInit, SMSExit, SMSFrame, SMSDraw, SMSScan, &SMSPaletteRecalc, 0x1E00,
+	256, 192, 4, 3
+};

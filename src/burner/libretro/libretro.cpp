a5d88e796bf20643bda867cd634008d0bfb6eab6
barbudreadmon <barbudreadmon@users.noreply.github.com>
2020-06-08 07:51:43 +0200

    (libretro) MemCard support

--- src/burner/libretro/libretro.cpp
+++ src/burner/libretro/libretro.cpp
@@ -154,6 +154,12 @@ char g_rom_parent_dir[MAX_PATH];
 char g_save_dir[MAX_PATH];
 char g_system_dir[MAX_PATH];
 char g_autofs_path[MAX_PATH];
+
+// MemCard support
+static TCHAR szMemoryCardFile[MAX_PATH];
+static int nMinVersion;
+static bool bMemCardFC1Format;
+
 extern unsigned int (__cdecl *BurnHighCol) (signed int r, signed int g, signed int b, signed int i);
 
 static bool driver_inited;
@@ -1132,6 +1138,164 @@ static void extract_directory(char *buf, const char *path, size_t size)
 	}
 }
 
+// MemCard support
+static int MemCardRead(TCHAR* szFilename, unsigned char* pData, int nSize)
+{
+	const char* szHeader  = "FB1 FC1 ";				// File + chunk identifier
+	char szReadHeader[8] = "";
+
+	bMemCardFC1Format = false;
+
+	FILE* fp = fopen(szFilename, _T("rb"));
+	if (fp == NULL) {
+		return 1;
+	}
+
+	fread(szReadHeader, 1, 8, fp);					// Read identifiers
+	if (memcmp(szReadHeader, szHeader, 8) == 0) {
+
+		// FB Alpha memory card file
+
+		int nChunkSize = 0;
+		int nVersion = 0;
+
+		bMemCardFC1Format = true;
+
+		fread(&nChunkSize, 1, 4, fp);				// Read chunk size
+		if (nSize < nChunkSize - 32) {
+			fclose(fp);
+			return 1;
+		}
+
+		fread(&nVersion, 1, 4, fp);					// Read version
+		if (nVersion < nMinVersion) {
+			fclose(fp);
+			return 1;
+		}
+		fread(&nVersion, 1, 4, fp);
+#if 0
+		if (nVersion < nBurnVer) {
+			fclose(fp);
+			return 1;
+		}
+#endif
+
+		fseek(fp, 0x0C, SEEK_CUR);					// Move file pointer to the start of the data block
+
+		fread(pData, 1, nChunkSize - 32, fp);		// Read the data
+	} else {
+
+		// MAME or old FB Alpha memory card file
+
+		unsigned char* pTemp = (unsigned char*)malloc(nSize >> 1);
+
+		memset(pData, 0, nSize);
+		fseek(fp, 0x00, SEEK_SET);
+
+		if (pTemp) {
+			fread(pTemp, 1, nSize >> 1, fp);
+
+			for (int i = 1; i < nSize; i += 2) {
+				pData[i] = pTemp[i >> 1];
+			}
+
+			free(pTemp);
+			pTemp = NULL;
+		}
+	}
+
+	fclose(fp);
+
+	return 0;
+}
+
+static int MemCardWrite(TCHAR* szFilename, unsigned char* pData, int nSize)
+{
+	FILE* fp = _tfopen(szFilename, _T("wb"));
+	if (fp == NULL) {
+		return 1;
+	}
+
+	if (bMemCardFC1Format) {
+
+		// FB Alpha memory card file
+
+		const char* szFileHeader  = "FB1 ";				// File identifier
+		const char* szChunkHeader = "FC1 ";				// Chunk identifier
+		const int nZero = 0;
+
+		int nChunkSize = nSize + 32;
+
+		fwrite(szFileHeader, 1, 4, fp);
+		fwrite(szChunkHeader, 1, 4, fp);
+
+		fwrite(&nChunkSize, 1, 4, fp);					// Chunk size
+
+		fwrite(&nBurnVer, 1, 4, fp);					// Version of FBA this was saved from
+		fwrite(&nMinVersion, 1, 4, fp);					// Min version of FBA data will work with
+
+		fwrite(&nZero, 1, 4, fp);						// Reserved
+		fwrite(&nZero, 1, 4, fp);						//
+		fwrite(&nZero, 1, 4, fp);						//
+
+		fwrite(pData, 1, nSize, fp);
+	} else {
+
+		// MAME or old FB Alpha memory card file
+
+		unsigned char* pTemp = (unsigned char*)malloc(nSize >> 1);
+		if (pTemp) {
+			for (int i = 1; i < nSize; i += 2) {
+				pTemp[i >> 1] = pData[i];
+			}
+
+			fwrite(pTemp, 1, nSize >> 1, fp);
+
+			free(pTemp);
+			pTemp = NULL;
+		}
+	}
+
+	fclose(fp);
+
+	return 0;
+}
+
+static int MemCardDoInsert(struct BurnArea* pba)
+{
+	if (MemCardRead(szMemoryCardFile, (unsigned char*)pba->Data, pba->nLen)) {
+		return 1;
+	}
+
+	return 0;
+}
+
+static int MemCardDoEject(struct BurnArea* pba)
+{
+	if (MemCardWrite(szMemoryCardFile, (unsigned char*)pba->Data, pba->nLen) == 0) {
+		return 0;
+	}
+
+	return 1;
+}
+
+static int MemCardInsert()
+{
+	BurnAcb = MemCardDoInsert;
+	BurnAreaScan(ACB_WRITE | ACB_MEMCARD, &nMinVersion);
+
+	return 0;
+}
+
+static int MemCardEject()
+{
+	BurnAcb = MemCardDoEject;
+	nMinVersion = 0;
+	BurnAreaScan(ACB_READ | ACB_MEMCARD, &nMinVersion);
+
+	return 0;
+}
+
 static bool retro_load_game_common()
 {
 	const char *dir = NULL;
@@ -1192,6 +1356,13 @@ static bool retro_load_game_common()
 		}
 
 		is_neogeo_game = ((BurnDrvGetHardwareCode() & HARDWARE_PUBLIC_MASK) == HARDWARE_SNK_NEOGEO);
+
+		if (is_neogeo_game)
+		{
+			// Initialize MemCard path
+			snprintf (szMemoryCardFile, sizeof(szMemoryCardFile), "%s%cfbneo%c%s.memcard", g_save_dir, path_default_slash_c(), path_default_slash_c(), g_driver_name);
+			MemCardInsert();
+		}
 
 		// Define nMaxPlayers early;
 		nMaxPlayers = BurnDrvGetMaxPlayers();
@@ -1425,6 +1596,8 @@ void retro_unload_game(void)
 {
 	if (driver_inited)
 	{
+		if (is_neogeo_game)
+			MemCardEject();
 		if (BurnStateSave(g_autofs_path, 0) == 0 && path_is_valid(g_autofs_path))
 			log_cb(RETRO_LOG_INFO, "[FBNEO] EEPROM succesfully saved to %s\n", g_autofs_path);
 		if (pVidImage) {

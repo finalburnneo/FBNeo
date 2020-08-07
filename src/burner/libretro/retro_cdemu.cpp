4779a078b91e0e41e8afd085d9d6f5a76954eea1
barbudreadmon <barbudreadmon@users.noreply.github.com>
2020-08-07 11:36:37 +0200

    neocd: oops

--- src/burner/libretro/retro_cdemu.cpp
+++ src/burner/libretro/retro_cdemu.cpp
@@ -875,8 +875,10 @@ static int cdimgGetSoundBuffer(short* buffer, int samples)
 
 		for (int i = samples * 2 - 1; i > 0; )
 		{
-			short tmpsrc = BURN_ENDIAN_SWAP_INT16(src[i]);
+			short tmpsrc;
+			tmpsrc = BURN_ENDIAN_SWAP_INT16(src[i]);
 			dst[i] = CLIP(tmpsrc + dst[i]); i--;
+			tmpsrc = BURN_ENDIAN_SWAP_INT16(src[i]);
 			dst[i] = CLIP(tmpsrc + dst[i]); i--;
 		}
 

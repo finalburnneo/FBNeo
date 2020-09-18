39a2d8e17ba0d0862a30a556bcae173d0707aeba
dinkc64 <terra79pin@yahoo.com>
2020-09-18 11:02:30 -0400

    Win UI: input macro and autofire updates [taoenwen]

--- src/burner/win32/inpd.cpp
+++ src/burner/win32/inpd.cpp
@@ -209,7 +209,7 @@ int InpdListMake(int bBuild)
 	if (bBuild)	{
 		SendMessage(hInpdList, LVM_DELETEALLITEMS, 0, 0);
 	}
-
+	
 	// Add all the input names to the list
 	for (unsigned int i = 0; i < nGameInpCount; i++) {
 		struct BurnInputInfo bii;
@@ -228,7 +228,7 @@ int InpdListMake(int bBuild)
 		}
 
 		memset(&LvItem, 0, sizeof(LvItem));
-		LvItem.mask = LVIF_TEXT | LVIF_PARAM;
+		LvItem.mask = LVIF_TEXT |  LVIF_PARAM;
 		LvItem.iItem = j;
 		LvItem.iSubItem = 0;
 		LvItem.pszText = ANSIToTCHAR(bii.szName, NULL, 0);
@@ -812,8 +812,17 @@ static INT_PTR CALLBACK DialogProc(HWND hDlg, UINT Msg, WPARAM wParam, LPARAM lP
 			int nCount = SendMessage(hInpdList, LVM_GETITEMCOUNT, 0, 0);
 
 			if (nCount > 0)
-				for (int i = 0; i < nCount; i++, pgi++)
-					pgi->Macro.nSysMacro = ListView_GetCheckState(hInpdList, i) ? 15 : 0;  // Setting Auto-Fire
+				for (int i = 0; i < nCount; i++, pgi++) {
+					// Setting Auto-Fire
+					pgi->Macro.nSysMacro = ListView_GetCheckState(hInpdList, i) ? 15 : 0;
+					// Exclude system macros
+					if (_stricmp("System Pause", pgi->Macro.szName) == 0 ||
+						_stricmp("System FFWD", pgi->Macro.szName) == 0 ||
+						_stricmp("System Load State", pgi->Macro.szName) == 0 ||
+						_stricmp("System Save State", pgi->Macro.szName) == 0 ||
+						_stricmp("System UNDO State", pgi->Macro.szName) == 0)
+						pgi->Macro.nSysMacro = 1;
+				}
 
 			SendMessage(hDlg, WM_CLOSE, 0, 0);
 			return 0;
@@ -958,9 +967,6 @@ static INT_PTR CALLBACK DialogProc(HWND hDlg, UINT Msg, WPARAM wParam, LPARAM lP
 		int Id = LOWORD(wParam);
 		NMHDR* pnm = (NMHDR*)lParam;
 
-		LPNMLISTVIEW pnmlv = (LPNMLISTVIEW)lParam;
-		int nCheckbox = pnmlv->iItem;  // Returns the line number of the mouse click
-
 		struct GameInp* pgi = NULL;
 		struct BurnInputInfo bii;
 
@@ -973,21 +979,35 @@ static INT_PTR CALLBACK DialogProc(HWND hDlg, UINT Msg, WPARAM wParam, LPARAM lP
 				ListItemDelete();
 			}
 		}
-		if (Id == IDC_INPD_LIST && pnm->code == LVN_ITEMCHANGED && ListView_GetCheckState(hInpdList, nCheckbox)) {  // Only check the trigger status of the checkbox.
-			pgi = GameInp + nCheckbox;
+		if (Id == IDC_INPD_LIST && pnm->code == LVN_ITEMCHANGED) {
+			/* Clear the checkboxs before the non Marco buttons 
+			   After that, you should not access these checkboxes that have been eliminated
+			   Otherwise, the program will throw an exception due to incorrect access */
+			NMLISTVIEW* pNMListView = (NMLISTVIEW*)pnm;
+			pgi = GameInp + pNMListView->iItem;
+
+			LVITEM LvItem;
+			memset(&LvItem, 0, sizeof(LvItem));
+
 			memset(&bii, 0, sizeof(bii));
-			BurnDrvGetInputInfo(&bii, nCheckbox); 
+			BurnDrvGetInputInfo(&bii, pNMListView->iItem);
 
-			INT32 nResult = (pgi->Input.pVal ? 0 : 1) + ((pgi->Macro.nSysMacro == 1 || bii.szName) ? 2 : 0);  // Is input specified + Is it a target macro
-			LPCWSTR psResult = (nResult > 1) ? L"This is a general input, not a macro." : L"You must first specify an input for the macro.";
+			if (pgi->Macro.nSysMacro == 1 || bii.szName) {
+				LvItem.iItem = pNMListView->iItem;
+				LvItem.mask = LVIF_STATE;
+				LvItem.stateMask = LVIS_STATEIMAGEMASK;
+				LvItem.state = 0;
 
-			// Only macrcos with assigned input keys are allowed.
-			if (nResult > 0) {
-				MessageBox(hInpdDlg, psResult, NULL, MB_ICONWARNING);
-				ListView_SetCheckState(hInpdList, nCheckbox, 0);
+				SendMessage(hInpdList, LVM_SETITEM, 0, (LPARAM)&LvItem);
 			}
+			// Avoid accessing checkboxes that have been eliminated
+			if (! pgi->Input.pVal && pgi->Macro.nSysMacro !=1 && pgi->Macro.szName)
+				// Check that the checkbox is properly checked
+				if (ListView_GetCheckState(hInpdList, pNMListView->iItem)){
+					ListView_SetCheckState(hInpdList, pNMListView->iItem, 0);
+					MessageBox(hInpdDlg, FBALoadStringEx(hAppInst, IDS_ERR_MARCO_NOT_MAPPING, true), NULL, MB_ICONWARNING);
+				}
 		}
-
 		if (Id == IDC_INPD_LIST && pnm->code == NM_CUSTOMDRAW) {
 			NMLVCUSTOMDRAW* plvcd = (NMLVCUSTOMDRAW*)lParam;
 

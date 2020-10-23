e41a0268398dcd658a2df6410dd4bd1e3604e930
dinkc64 <terra79pin@yahoo.com>
2020-10-22 20:15:44 -0400

    megadrive: add Mad Stalker

--- src/burn/drv/megadrive/d_megadrive.cpp
+++ src/burn/drv/megadrive/d_megadrive.cpp
@@ -45213,3 +45213,21 @@ struct BurnDriver BurnDrvmd_landstlkc = {
 	MegadriveInit, MegadriveExit, MegadriveFrame, MegadriveDraw, MegadriveScan,
 	&bMegadriveRecalcPalette, 0x100, 320, 224, 4, 3
 };
+
+// Mad Stalker - Full Metal Forth
+static struct BurnRomInfo md_madstalkerRomDesc[] = {
+	{ "Mad Stalker - Full Metal Forth", 2097152, 0x64e7f1cd, BRF_PRG | SEGA_MD_ROM_LOAD16_WORD_SWAP | SEGA_MD_ROM_OFFS_000000  },
+};
+
+STD_ROM_PICK(md_madstalker)
+STD_ROM_FN(md_madstalker)
+
+struct BurnDriver BurnDrvmd_madstalker = {
+	"md_madstalker", NULL, NULL, NULL, "2020",
+	"Mad Stalker - Full Metal Forth\0", NULL, "OPERAHOUSE", "Sega Megadrive",
+	NULL, NULL, NULL, NULL,
+	BDF_GAME_WORKING | BDF_16BIT_ONLY | BDF_HOMEBREW, 1, HARDWARE_SEGA_MEGADRIVE, GBF_PLATFORM, 0,
+	MegadriveGetZipName, md_madstalkerRomInfo, md_madstalkerRomName, NULL, NULL, NULL, NULL, MegadriveInputInfo, MegadriveDIPInfo,
+	MegadriveInit, MegadriveExit, MegadriveFrame, MegadriveDraw, MegadriveScan,
+	&bMegadriveRecalcPalette, 0x100, 320, 224, 4, 3
+};

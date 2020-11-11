265b672828b9339eb85de480839361e0749b4fe2
Svipur <71509002+Svipur@users.noreply.github.com>
2020-11-12 01:23:11 +0300

    48k-only fixes.

--- src/burn/drv/spectrum/d_spectrum.cpp
+++ src/burn/drv/spectrum/d_spectrum.cpp
@@ -17193,16 +17193,16 @@ static struct BurnRomInfo SpecSuperheroRomDesc[] = {
 	{ "Super Hero (1988)(Codemasters).tap", 51573, 0x26d19439, BRF_ESS | BRF_PRG },
 };
 
-STDROMPICKEXT(SpecSuperhero, SpecSuperhero, Spec128)
+STDROMPICKEXT(SpecSuperhero, SpecSuperhero, Spectrum)
 STD_ROM_FN(SpecSuperhero)
 
 struct BurnDriver BurnSpecSuperhero = {
-	"spec_superhero", NULL, "spec_spec128", NULL, "1988",
+	"spec_superhero", NULL, "spec_spectrum", NULL, "1988",
 	"Super Hero\0", NULL, "Codemasters", "ZX Spectrum",
 	NULL, NULL, NULL, NULL,
 	BDF_GAME_WORKING, 1, HARDWARE_SPECTRUM, GBF_MISC, 0,
 	SpectrumGetZipName, SpecSuperheroRomInfo, SpecSuperheroRomName, NULL, NULL, NULL, NULL, SpecInputInfo, SpecDIPInfo,
-	Spec128KInit, SpecExit, SpecFrame, SpecDraw, SpecScan,
+	SpecInit, SpecExit, SpecFrame, SpecDraw, SpecScan,
 	&SpecRecalc, 0x10, 288, 224, 4, 3
 };
 

9b33dd3e99f5ed64a9f7c10e1c79d4e115d9972b
dinkc64 <terra79pin@yahoo.com>
2021-01-14 20:05:39 -0500

    midway, fix nba game slowness

--- src/burn/drv/midway/midtunit_dma.h
+++ src/burn/drv/midway/midtunit_dma.h
@@ -319,6 +319,7 @@ DECLARE_BLITTER_SET(dma_draw_noskip_noscale,   dma_state->bpp, EXTRACTGEN,   SKI
 static void TUnitDmaCallback()
 {
 	TMS34010GenerateIRQ(DMA_IRQ);
+	TMS34010RunEnd();
 	nDMA[DMA_COMMAND] &= ~0x8000;
 }
 

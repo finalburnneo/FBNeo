09ee90e8710531de4a9a9e31f008d17bda8ce0f6
iq_132 <d2vanvoo@gmail.com>
2021-03-03 23:50:39 -0500

    Add external read/write for ymf271.

--- src/burn/snd/ymf271.h
+++ src/burn/snd/ymf271.h
@@ -5,6 +5,8 @@ void ymf271_init(int clock, UINT8 *rom, INT32 romsize, void (*irq_cb)(INT32, INT
 void ymf271_exit();
 void ymf271_reset();
 
+void ymf271_set_external_handlers(UINT8 (*read)(UINT32), void (*write)(UINT32, UINT8));
+
 void ymf271_update(INT16 **buffers, int samples);
 INT32 ymf271_timer_over(INT32 id);
 UINT8 ymf271_read(INT32 offset);

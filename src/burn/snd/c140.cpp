4cc73f5b79b9ac4b9825f83e2e1bfb623ff552a6
dinkc64 <terra79pin@yahoo.com>
2021-05-21 01:31:50 -0400

    c140 fix for namcos2 engine [Angelo Salese]

--- src/burn/snd/c140.cpp
+++ src/burn/snd/c140.cpp
@@ -498,6 +498,10 @@ UINT8 c140_read(UINT16 offset)
 {
 	UpdateStream(0);
 	offset &= 0x1ff;
+	if ((offset & 0xf) == 0x5) {
+		C140_VOICE const &v = m_voi[offset >> 4];
+		return (v.key ? 0x40 : 0x00) | (v.mode & 0x3f);
+	}
 	return m_REG[offset];
 }
 

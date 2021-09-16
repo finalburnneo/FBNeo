5523203843a33863661503d6ebb937697a650d9b
dinkc64 <terra79pin@yahoo.com>
2021-09-16 00:04:26 -0400

    update for softfx

--- src/intf/video/scalers/xbr.h
+++ src/intf/video/scalers/xbr.h
@@ -1,6 +1,7 @@
 #ifndef _XBR_H
 #define _XBR_H
 
+// 16bit
 void xbr2x_a(unsigned char * pIn,  unsigned int srcPitch, unsigned char * pOut, unsigned int dstPitch, int Xres, int Yres);
 void xbr2x_b(unsigned char * pIn,  unsigned int srcPitch, unsigned char * pOut, unsigned int dstPitch, int Xres, int Yres);
 void xbr2x_c(unsigned char * pIn,  unsigned int srcPitch, unsigned char * pOut, unsigned int dstPitch, int Xres, int Yres);
@@ -12,5 +13,10 @@ void xbr3x_c(unsigned char * pIn,  unsigned int srcPitch, unsigned char * pOut, 
 void xbr4x_a(unsigned char * pIn,  unsigned int srcPitch, unsigned char * pOut, unsigned int dstPitch, int Xres, int Yres);
 void xbr4x_b(unsigned char * pIn,  unsigned int srcPitch, unsigned char * pOut, unsigned int dstPitch, int Xres, int Yres);
 void xbr4x_c(unsigned char * pIn,  unsigned int srcPitch, unsigned char * pOut, unsigned int dstPitch, int Xres, int Yres);
+
+// 32bit
+void xbr2x_32(unsigned char * pIn,  unsigned int srcPitch, unsigned char * pOut, unsigned int dstPitch, int Xres, int Yres);
+void xbr3x_32(unsigned char * pIn,  unsigned int srcPitch, unsigned char * pOut, unsigned int dstPitch, int Xres, int Yres);
+void xbr4x_32(unsigned char * pIn,  unsigned int srcPitch, unsigned char * pOut, unsigned int dstPitch, int Xres, int Yres);
 
 #endif

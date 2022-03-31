02e8b3372b62e3d3633908ed1b932b491d10df2c
Matt <92021977+matead92@users.noreply.github.com>
2022-03-31 21:04:56 +1100

    Renamed UMK3PB2 to UMK3P
    Renamed UMK3PB2 to UMK3P

--- src/burn/drv/midway/d_wunit.cpp
+++ src/burn/drv/midway/d_wunit.cpp
@@ -1018,7 +1018,7 @@ struct BurnDriver BurnDrvUmk3uc = {
 
 // Ultimate Mortal Kombat 3 Plus (Hack, Beta 2)
 // Author: zpaul2fresh8
-static struct BurnRomInfo umk3pb2RomDesc[] = {
+static struct BurnRomInfo umk3pRomDesc[] = {
 	{ "l1.2_mortal_kombat_3_u54_ultimate.u54",				0x080000, 0x54A5359F, 1 | BRF_PRG | BRF_ESS }, //  0 TMS34010
 	{ "l1.2_mortal_kombat_3_u63_ultimate.u63",				0x080000, 0xFB83320D, 1 | BRF_PRG | BRF_ESS }, //  1
 
@@ -1060,15 +1060,15 @@ static struct BurnRomInfo umk3pb2RomDesc[] = {
 	{ "463_mk3_ultimate.u64",								0x1009, 0x4f425218, 0 | BRF_OPT },
 };
 
-STD_ROM_PICK(umk3pb2)
-STD_ROM_FN(umk3pb2)
+STD_ROM_PICK(umk3p)
+STD_ROM_FN(umk3p)
 
-struct BurnDriver BurnDrvUmk3pb2 = {
-	"umk3pb2", "umk3", NULL, NULL, "2022",
+struct BurnDriver BurnDrvUmk3p = {
+	"umk3p", "umk3", NULL, NULL, "2022",
 	"Ultimate Mortal Kombat 3 PLUS (BETA 2)\0", NULL, "Midway", "MIDWAY Wolf-Unit",
 	NULL, NULL, NULL, NULL,
 	BDF_GAME_WORKING | BDF_CLONE | BDF_HACK, 2, HARDWARE_MIDWAY_WUNIT, GBF_MISC, 0,
-	NULL, umk3pb2RomInfo, umk3pb2RomName, NULL, NULL, NULL, NULL, Mk3InputInfo, Mk3DIPInfo,
+	NULL, umk3pRomInfo, umk3pRomName, NULL, NULL, NULL, NULL, Mk3InputInfo, Mk3DIPInfo,
     WolfUnitInit, WolfUnitExit, WolfUnitFrame, WolfUnitDraw, WolfUnitScan, &nWolfUnitRecalc, 0x8000,
     400, 256, 4, 3
 };

8717777869a361deb837a35e2b952674526a1a38
dinkc64 <terra79pin@yahoo.com>
2022-08-03 00:47:07 -0400

    thready, tweak for alt. use

--- src/burn/devices/thready.h
+++ src/burn/devices/thready.h
@@ -6,6 +6,7 @@
 #define THREADY_0THREAD 3 // neither of the above.. (no threading!)
 
 #define STARTUP_FRAMES 180
+#define THREADY_CHECK_CB_TIME 0
 
 #if defined(WIN32)
 #define THREADY THREADY_WINDOWS
@@ -25,7 +26,7 @@
 #endif
 
 #if (THREADY == THREADY_WINDOWS)
-long unsigned int __stdcall ThreadyProc(void*);
+static long unsigned int __stdcall ThreadyProc(void*);
 
 struct threadystruct
 {
@@ -44,6 +45,7 @@ struct threadystruct
 		thready_ok = 0;
 		ok_to_thread = 0;
 		ok_to_wait = 0;
+		startup_frame = 0;
 
 		our_callback = thread_callback;
 
@@ -93,7 +95,7 @@ struct threadystruct
 		SCAN_VAR(startup_frame);
 	}
 
-	void reset() {
+	void reset() { // only call this if using for epic12
 		startup_frame = STARTUP_FRAMES;
 	}
 
@@ -126,12 +128,20 @@ struct threadystruct
 
 static threadystruct thready;
 
-long unsigned int __stdcall ThreadyProc(void*) {
+static long unsigned int __stdcall ThreadyProc(void*) {
 	do {
 		DWORD dwWaitResult = WaitForSingleObject(thready.our_event, INFINITE);
 
 		if (dwWaitResult == WAIT_OBJECT_0 && thready.end_thread == 0) {
+#if THREADY_CHECK_CB_TIME
+			time_t begin_time = clock();
+#endif
 			thready.our_callback();
+#if THREADY_CHECK_CB_TIME
+			time_t end_time = clock();
+			double duration = (double)(end_time - begin_time) / CLOCKS_PER_SEC;
+			bprintf(0, _T("thready: callback took %2.3f seconds\n"), duration);
+#endif
 			SetEvent(thready.wait_event);
 		} else {
 			SetEvent(thready.wait_event);

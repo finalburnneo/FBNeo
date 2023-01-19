71c0fc6c449aea300d7140f86597f483c53e4ecc
Igor-Arabe <73916998+Igor-Arabe@users.noreply.github.com>
2023-01-18 22:35:12 -0300

    d_megadrive: add fightvengt (#1310)
    Tournament Edition. Rebalanced and updated version by original author [Oruam]
    'md_fightveng' marked as clone.

--- src/burn/drv/megadrive/d_megadrive.cpp
+++ src/burn/drv/megadrive/d_megadrive.cpp
@@ -43781,6 +43781,24 @@ struct BurnDriver BurnDrvmd_cannonfire = {
 	&bMegadriveRecalcPalette, 0x100, 320, 224, 4, 3
 };
 
+// Fight for Vengeance - Tournament Edition (HB)
+static struct BurnRomInfo md_fightvengtRomDesc[] = {
+	{ "Fight for Vengeance - Tournament Edition (2022)(Genesis Fight Engine).bin", 4194304, 0x813a61a6, BRF_PRG | SEGA_MD_ROM_LOAD16_WORD_SWAP | SEGA_MD_ROM_OFFS_000000  },
+};
+
+STD_ROM_PICK(md_fightvengt)
+STD_ROM_FN(md_fightvengt)
+
+struct BurnDriver BurnDrvmd_fightvengt = {
+	"md_fightvengt", NULL, NULL, NULL, "2022",
+	"Fight for Vengeance - Tournament Edition (HB)\0", NULL, "Genesis Fight Engine", "Sega Megadrive",
+	NULL, NULL, NULL, NULL,
+	BDF_GAME_WORKING | BDF_16BIT_ONLY | BDF_HOMEBREW, 2, HARDWARE_SEGA_MEGADRIVE, GBF_VSFIGHT, 0,
+	MegadriveGetZipName, md_fightvengtRomInfo, md_fightvengtRomName, NULL, NULL, NULL, NULL, MegadriveInputInfo, MegadriveDIPInfo,
+	MegadriveInit, MegadriveExit, MegadriveFrame, MegadriveDraw, MegadriveScan,
+	&bMegadriveRecalcPalette, 0x100, 320, 224, 4, 3
+};
+
 // Fight for Vengeance (HB)
 static struct BurnRomInfo md_fightvengRomDesc[] = {
 	{ "Fight for Vengeance (2021)(PSCD Games).bin", 4194304, 0x7db61291, BRF_PRG | SEGA_MD_ROM_LOAD16_WORD_SWAP | SEGA_MD_ROM_OFFS_000000  },
@@ -43790,10 +43808,10 @@ STD_ROM_PICK(md_fightveng)
 STD_ROM_FN(md_fightveng)
 
 struct BurnDriver BurnDrvmd_fightveng = {
-	"md_fightveng", NULL, NULL, NULL, "2021",
+	"md_fightveng", "md_fightvengt", NULL, NULL, "2021",
 	"Fight for Vengeance (HB)\0", NULL, "PSCD Games", "Sega Megadrive",
 	NULL, NULL, NULL, NULL,
-	BDF_GAME_WORKING | BDF_16BIT_ONLY | BDF_HOMEBREW, 2, HARDWARE_SEGA_MEGADRIVE, GBF_VSFIGHT, 0,
+	BDF_GAME_WORKING | BDF_16BIT_ONLY | BDF_CLONE | BDF_HOMEBREW, 2, HARDWARE_SEGA_MEGADRIVE, GBF_VSFIGHT, 0,
 	MegadriveGetZipName, md_fightvengRomInfo, md_fightvengRomName, NULL, NULL, NULL, NULL, MegadriveInputInfo, MegadriveDIPInfo,
 	MegadriveInit, MegadriveExit, MegadriveFrame, MegadriveDraw, MegadriveScan,
 	&bMegadriveRecalcPalette, 0x100, 320, 224, 4, 3

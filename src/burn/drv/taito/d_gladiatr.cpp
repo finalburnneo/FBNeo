27e2f9145c1daa874df7924e7308b1808bd4880a
taoenwen <67533945+taoenwen@users.noreply.github.com>
2023-01-29 10:20:18 +0800

    d_gladiatr: fix a coin issue
    d_gladiatr: add Ougon no Shiro (Japan)

--- src/burn/drv/taito/d_gladiatr.cpp
+++ src/burn/drv/taito/d_gladiatr.cpp
@@ -129,16 +129,16 @@ static struct BurnDIPInfo GladiatrDIPList[]=
 	{0x00, 0x01, 0x80, 0x00, "On"						},
 
 	{0   , 0xfe, 0   ,    4, "Coin A"					},
-	{0x01, 0x01, 0x03, 0x03, "1 Coin  1 Credits"		},
+	{0x01, 0x01, 0x03, 0x03, "1 Coin  1 Credit"			},
 	{0x01, 0x01, 0x03, 0x02, "1 Coin  2 Credits"		},
 	{0x01, 0x01, 0x03, 0x01, "1 Coin  4 Credits"		},
 	{0x01, 0x01, 0x03, 0x00, "1 Coin  5 Credits"		},
 
 	{0   , 0xfe, 0   ,    4, "Coin B"					},
-	{0x01, 0x01, 0x0c, 0x00, "5 Coins 1 Credits"		},
-	{0x01, 0x01, 0x0c, 0x04, "4 Coins 1 Credits"		},
-	{0x01, 0x01, 0x0c, 0x08, "3 Coins 1 Credits"		},
-	{0x01, 0x01, 0x0c, 0x0c, "2 Coins 1 Credits"		},
+	{0x01, 0x01, 0x0c, 0x00, "5 Coins 1 Credit"			},
+	{0x01, 0x01, 0x0c, 0x04, "4 Coins 1 Credit"			},
+	{0x01, 0x01, 0x0c, 0x08, "3 Coins 1 Credit"			},
+	{0x01, 0x01, 0x0c, 0x0c, "2 Coins 1 Credit"			},
 
 	{0   , 0xfe, 0   ,    2, "Free Play"				},
 	{0x01, 0x01, 0x10, 0x10, "Off"						},
@@ -761,6 +761,8 @@ static void region_swap()
 
 static INT32 DrvInit()
 {
+	cflyball_hack = false;
+
 	BurnAllocMemIndex();
 
 	{
@@ -1204,6 +1206,59 @@ struct BurnDriver BurnDrvGladiatr = {
 	NULL, NULL, NULL, NULL,
 	BDF_GAME_WORKING, 2, HARDWARE_TAITO_MISC, GBF_SCRFIGHT, 0,
 	NULL, gladiatrRomInfo, gladiatrRomName, NULL, NULL, NULL, NULL, GladiatrInputInfo, GladiatrDIPInfo,
+	DrvInit, DrvExit, DrvFrame, DrvDraw, DrvScan, &BurnRecalc, 0x401,
+	256, 224, 4, 3
+};
+
+
+// Ougon no Shiro (Japan)
+
+static struct BurnRomInfo ogonsiroRomDesc[] = {
+	{ "qb0_5",			0x4000, 0x25b19efb, 1 | BRF_PRG | BRF_ESS },  //  0 Z80 #0 Code
+	{ "qb0_4",			0x2000, 0x347ec794, 1 | BRF_PRG | BRF_ESS },  //  1
+	{ "qb0_1",			0x4000, 0x040c9839, 1 | BRF_PRG | BRF_ESS },  //  2
+	{ "qc0_3",			0x8000, 0xd6a342e7, 1 | BRF_PRG | BRF_ESS },  //  3
+
+	{ "qb0_17",			0x4000, 0xe78be010, 2 | BRF_PRG | BRF_ESS },  //  4 Z80 #1 Code
+
+	{ "qb0_20",			0x8000, 0x15916eda, 3 | BRF_PRG | BRF_ESS },  //  5 M6809 Code
+	{ "qb0_19",			0x8000, 0x79caa7ed, 3 | BRF_PRG | BRF_ESS },  //  6
+	{ "qb0_18",			0x8000, 0xe9591260, 3 | BRF_PRG | BRF_ESS },  //  7
+
+	{ "qc0_15",			0x2000, 0x5e1332b8, 4 | BRF_GRA },            //  8 Characters
+
+	{ "qb0_12",			0x8000, 0x0585d9ac, 5 | BRF_GRA },            //  9 Background Tiles
+	{ "qb0_13",			0x8000, 0xa6bb797b, 5 | BRF_GRA },            // 10
+	{ "qb0_14",			0x8000, 0x85b71211, 5 | BRF_GRA },            // 11
+
+	{ "qc1_6",			0x4000, 0x1a2bc769, 6 | BRF_GRA },            // 12 Sprites
+	{ "qc2_7",			0x8000, 0x4b677bd9, 6 | BRF_GRA },            // 13
+	{ "qc0_8",			0x4000, 0x1c7ffdad, 6 | BRF_GRA },            // 14
+	{ "qc1_9",			0x4000, 0x38f5152d, 6 | BRF_GRA },            // 15
+	{ "qc1_10",			0x8000, 0x87ab6cc4, 6 | BRF_GRA },            // 16
+	{ "qc2_11",			0x8000, 0x25eaa4ff, 6 | BRF_GRA },            // 17
+
+	{ "q3.2b",			0x0020, 0x6a7c3c60, 7 | BRF_GRA },            // 18 Unused PROMS
+	{ "q4.5s",			0x0020, 0xe325808e, 7 | BRF_GRA },            // 19
+
+	{ "aq_002.9b",		0x0400, 0xb30d225f, 8 | BRF_GRA },            // 20 CCTL MCU Code
+
+	{ "aq_003.xx",		0x0400, 0x1d02cd5f, 9 | BRF_PRG | BRF_ESS },  // 21 CCPU MCU Code
+
+	{ "aq_006.3a",		0x0400, 0x3c5ca4c6, 10 | BRF_PRG | BRF_ESS }, // 22 UCPU MCU Code
+
+	{ "aq_007.6c",		0x0800, 0xf19af04d, 11 | BRF_PRG | BRF_ESS }, // 23 CSND MCU Code
+};
+
+STD_ROM_PICK(ogonsiro)
+STD_ROM_FN(ogonsiro)
+
+struct BurnDriver BurnDrvOgonsiro = {
+	"ogonsiro", "gladiatr", NULL, NULL, "1986",
+	"Ougon no Shiro (Japan)\0", NULL, "Allumer / Taito Corporation", "Miscellaneous",
+	L"Ougon no Shiro (Japan)\0\u9ec4\u91d1\u306e\u57ce\0", NULL, NULL, NULL,
+	BDF_GAME_WORKING | BDF_CLONE, 2, HARDWARE_TAITO_MISC, GBF_SCRFIGHT, 0,
+	NULL, ogonsiroRomInfo, ogonsiroRomName, NULL, NULL, NULL, NULL, GladiatrInputInfo, GladiatrDIPInfo,
 	DrvInit, DrvExit, DrvFrame, DrvDraw, DrvScan, &BurnRecalc, 0x401,
 	256, 224, 4, 3
 };

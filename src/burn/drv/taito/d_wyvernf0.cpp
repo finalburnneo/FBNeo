50579a833498b6a44e5ce2f9438cb6b34d340173
barbudreadmon <barbudreadmon@users.noreply.github.com>
2023-02-01 13:15:50 +0100

    d_wyvernf0: hiscore support

--- src/burn/drv/taito/d_wyvernf0.cpp
+++ src/burn/drv/taito/d_wyvernf0.cpp
@@ -354,6 +354,8 @@ static INT32 DrvDoReset()
 
 	MSM5232Reset();
 
+	HiscoreReset();
+
 	return 0;
 }
 
@@ -748,7 +750,7 @@ struct BurnDriver BurnDrvWyvernf0 = {
 	"wyvernf0", NULL, NULL, NULL, "1985",
 	"Wyvern F-0 (Rev 1)\0", NULL, "Taito Corporation", "Miscellaneous",
 	NULL, NULL, NULL, NULL,
-	BDF_GAME_WORKING | BDF_ORIENTATION_VERTICAL, 2,  HARDWARE_TAITO_MISC, GBF_VERSHOOT, 0,
+	BDF_GAME_WORKING | BDF_ORIENTATION_VERTICAL | BDF_HISCORE_SUPPORTED, 2,  HARDWARE_TAITO_MISC, GBF_VERSHOOT, 0,
 	NULL, wyvernf0RomInfo, wyvernf0RomName, NULL, NULL, NULL, NULL, Wyvernf0InputInfo, Wyvernf0DIPInfo,
 	DrvInit, DrvExit, DrvFrame, DrvDraw, DrvScan, &DrvRecalc, 0x200,
 	224, 256, 3, 4
@@ -788,7 +790,7 @@ struct BurnDriver BurnDrvWyvernf0a = {
 	"wyvernf0a", "wyvernf0", NULL, NULL, "1985",
 	"Wyvern F-0\0", NULL, "Taito Corporation", "Miscellaneous",
 	NULL, NULL, NULL, NULL,
-	BDF_GAME_WORKING | BDF_CLONE | BDF_ORIENTATION_VERTICAL, 2,  HARDWARE_TAITO_MISC, GBF_VERSHOOT, 0,
+	BDF_GAME_WORKING | BDF_CLONE | BDF_ORIENTATION_VERTICAL | BDF_HISCORE_SUPPORTED, 2,  HARDWARE_TAITO_MISC, GBF_VERSHOOT, 0,
 	NULL, wyvernf0aRomInfo, wyvernf0aRomName, NULL, NULL, NULL, NULL, Wyvernf0InputInfo, Wyvernf0DIPInfo,
 	DrvInit, DrvExit, DrvFrame, DrvDraw, DrvScan, &DrvRecalc, 0x200,
 	224, 256, 3, 4

642c8a5be2369d1025317ee1d5d2a96b8d7a9dd8
dinkc64 <terra79pin@yahoo.com>
2023-03-09 00:41:03 -0500

    m6502, add special 1-opcode delayed nmi for nes

--- src/cpu/m6502/m6502.cpp
+++ src/cpu/m6502/m6502.cpp
@@ -219,6 +219,14 @@ void m6502_set_nmi_hold()
 	m6502.hold_nmi = 1;
 }
 
+// for nes - ppuctrl transition to 0x80 during vblank takes nmi after
+// next opcode has executed
+void m6502_set_nmi_hold2()
+{
+	m6502.hold_nmi = 1;
+	m6502.delay_nmi = 2;
+}
+
 M6502_INLINE void m6502_take_irq(void)
 {
 	if (m6502.nmi_req)
@@ -303,16 +311,21 @@ int m6502_execute(int cycles)
 //		debugger_instruction_hook(Machine, PCD);
 
 		/* if an irq is pending, take it now */
-		if( m6502.pending_irq == 1)
+
+		if (m6502.delay_nmi > 0) {
+			m6502.delay_nmi--;
+			if (m6502.delay_nmi == 0) {
+				m6502.nmi_req = 1;
+			}
+		}
+
+		if( m6502.pending_irq == 1 || m6502.nmi_req)
 			m6502_take_irq();
 
 		op = RDOP();
 
 		//(*m6502.insn[op])();
 		(*insnActive[op])();
-
-		if( m6502.nmi_req )
-			m6502_take_irq();
 
 		/* check if the I flag was just reset (interrupts enabled) */
 		if( m6502.after_cli )
@@ -335,7 +348,7 @@ int m6502_execute(int cycles)
 					m6502.pending_irq = 1;
 				}
 			}
-			if( m6502.pending_irq == 1 || m6502.nmi_req == 1 )
+			if( m6502.pending_irq == 1 )
 				m6502_take_irq();
 			if ( m6502.pending_irq == 2 ) {
 				m6502.pending_irq = 1;

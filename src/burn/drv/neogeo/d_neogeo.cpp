03c9498e84fc921599a3028c96d90140c3ad6cc3
Igor-Arabe <110349306+IcDasNMs@users.noreply.github.com>
2023-04-10 10:29:47 -0300

    d_neogeo: mslugxdg
    updated to latest version

--- src/burn/drv/neogeo/d_neogeo.cpp
+++ src/burn/drv/neogeo/d_neogeo.cpp
@@ -20697,8 +20697,8 @@ struct BurnDriver BurnDrvmslugxsc = {
 
 // Metal Slug X (Multifunction Hack)
 static struct BurnRomInfo mslugxdgRomDesc[] = {
-	{ "250-p1dg.p1",    0x100000, 0x4868b5ce, 1 | BRF_ESS | BRF_PRG }, //  0 68K code
-	{ "250-p2dg.ep1",   0x400000, 0xda239a0d, 1 | BRF_ESS | BRF_PRG }, //  1
+	{ "250-p1dg.p1",    0x100000, 0x89652f48, 1 | BRF_ESS | BRF_PRG }, //  0 68K code
+	{ "250-p2dg.ep1",   0x400000, 0x5f8275b3, 1 | BRF_ESS | BRF_PRG }, //  1
 
 	{ "250-s1.s1",      0x020000, 0xfb6f441d, 2 | BRF_GRA },           //  2 Text layer tiles
 
@@ -20721,7 +20721,7 @@ STD_ROM_FN(mslugxdg)
 
 struct BurnDriver BurnDrvmslugxdg = {
 	"mslugxdg", "mslugx", "neogeo", NULL, "2023",
-	"Metal Slug X (Multifunction Hack, 20230402)\0", NULL, "hack", "Neo Geo MVS",
+	"Metal Slug X (Multifunction Hack, 20230410)\0", NULL, "hack", "Neo Geo MVS",
 	NULL, NULL, NULL, NULL,
 	BDF_GAME_WORKING | BDF_CLONE | BDF_HACK | BDF_HISCORE_SUPPORTED, 2, HARDWARE_PREFIX_CARTRIDGE | HARDWARE_SNK_NEOGEO, GBF_RUNGUN, FBF_MSLUG,
 	NULL, mslugxdgRomInfo, mslugxdgRomName, NULL, NULL, NULL, NULL, neogeoInputInfo, neogeoDIPInfo,

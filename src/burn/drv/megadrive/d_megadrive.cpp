fdca1c84d45d0d6c4c62beb7853f9494106659aa
dinkc64 <terra79pin@yahoo.com>
2023-06-09 09:09:33 -0400

    megadrive update from gab75

--- src/burn/drv/megadrive/d_megadrive.cpp
+++ src/burn/drv/megadrive/d_megadrive.cpp
@@ -43409,6 +43409,24 @@ struct BurnDriver BurnDrvmd_pacman = {
 
 // -- Homebrew additions below --
 
+// Astebros (HB)
+static struct BurnRomInfo md_astebrosRomDesc[] = {
+	{ "Astebros (2023)(NeoFID Studios).bin", 15466496, 0x0833390c, BRF_PRG | SEGA_MD_ROM_LOAD16_WORD_SWAP | SEGA_MD_ROM_OFFS_000000  },
+};
+
+STD_ROM_PICK(md_astebros)
+STD_ROM_FN(md_astebros)
+
+struct BurnDriver BurnDrvmd_astebros = {
+	"md_astebros", NULL, NULL, NULL, "2023",
+	"Astebros (HB)\0", NULL, "NeoFID Studios", "Sega Megadrive",
+	NULL, NULL, NULL, NULL,
+	BDF_GAME_WORKING | BDF_16BIT_ONLY | BDF_HOMEBREW, 1, HARDWARE_SEGA_MEGADRIVE | HARDWARE_SEGA_MEGADRIVE_PCB_SSF2 | HARDWARE_SEGA_MEGADRIVE_PCB_SEGA_SRAM, GBF_ADV | GBF_PLATFORM, 0,
+	MegadriveGetZipName, md_astebrosRomInfo, md_astebrosRomName, NULL, NULL, NULL, NULL, MegadriveInputInfo, MegadriveDIPInfo,
+	MegadriveInit, MegadriveExit, MegadriveFrame, MegadriveDraw, MegadriveScan,
+	&bMegadriveRecalcPalette, 0x100, 320, 224, 4, 3
+};
+
 // Hellfire Saga - Sonic (GlobalHack)
 
 static struct BurnRomInfo md_hellfiresagaRomDesc[] = {

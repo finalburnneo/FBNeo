0a911d95543fcce3d9a6a1f2c11d005dbbad27b7
Igor-Arabe <110349306+IcDasNMs@users.noreply.github.com>
2023-06-11 21:21:13 -0300

    d_neogeo: samsho2pe
    Improvements 2023-06-11:
    - Haohmaru Every DP startup is reduced by 2 frames
    - Haohmaru DP+AB invincible in start
    - Wanfu DP+AB invincible in start
    - Charlotte Every DP startup is reduced by 2 frames
    - Genjuro Every DP startup is reduced by 2 frames
    - Hanzo Jump right+CD Jump left+CD, cancel with especial move "Ninja Windsplitter"

--- src/burn/drv/neogeo/d_neogeo.cpp
+++ src/burn/drv/neogeo/d_neogeo.cpp
@@ -17052,11 +17052,11 @@ struct BurnDriver BurnDrvSamsho2sp = {
 	0x1000, 320, 224, 4, 3
 };
 
-// Samurai Shodown II Perfect Hack - 2023-06-04
+// Samurai Shodown II Perfect Hack - 2023-06-11
 
 static struct BurnRomInfo samsho2peRomDesc[] = {
-	{ "063-p1pe.p1",    0x100000, 0x600a9df6, 1 | BRF_ESS | BRF_PRG }, //  0 68K code
-	{ "063-p2pe.sp2",   0x100000, 0x045e3d35, 1 | BRF_ESS | BRF_PRG }, //  1
+	{ "063-p1pe.p1",    0x100000, 0xa21d27ba, 1 | BRF_ESS | BRF_PRG }, //  0 68K code
+	{ "063-p2pe.sp2",   0x100000, 0x55e55b96, 1 | BRF_ESS | BRF_PRG }, //  1
 	{ "063-p3pe.p3",    0x020000, 0xefe257ce, 0 | BRF_ESS | BRF_PRG }, //  2
 
 	{ "063-s1.s1",      0x020000, 0x64a5cd66, 2 | BRF_GRA },           //  3 Text layer tiles

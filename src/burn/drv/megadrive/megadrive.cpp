3649a6ea067b68190be5b11408e9858641cef006
barbudreadmon <barbudreadmon@users.noreply.github.com>
2023-07-23 19:50:49 +0200

    megadrive: implement overclocking (2nd try)

--- src/burn/drv/megadrive/megadrive.cpp
+++ src/burn/drv/megadrive/megadrive.cpp
@@ -4659,6 +4659,9 @@ INT32 MegadriveFrame()
 
 	for (INT32 y=0; y<lines; y++) {
 
+		if (y > lines_vis && nBurnCPUSpeedAdjust > 0x100)
+			SekRunM68k((INT32)((INT64)CYCLES_M68K_LINE * (nBurnCPUSpeedAdjust - 0x100) / 0x0100));
+
 		Scanline = y;
 
 		if (y < lines_vis) {
@@ -4785,8 +4788,6 @@ INT32 MegadriveFrame()
 			bprintf(0, _T("line cycles[%d]: %d."), Scanline, SekCyclesLine());
 #endif
 	}
-
-	SekRunM68k((INT32)((INT64)CYCLES_M68K_LINE * (nBurnCPUSpeedAdjust - 0x100) / 0x0100));
 
 	if (pBurnDraw) MegadriveDraw();
 

c7e04b8137a72d9c50fd1604fe56b43267258634
Igor-Arabe <73916998+Igor-Arabe@users.noreply.github.com>
2023-07-29 21:15:03 -0300

    d_neogeo: samsho2pe (#1531)
    2021-07-29:
    Title screen with new pallete
    Genjuro  readjusted distance kyoyokujin+ab
    Haohmaru readjusted damage normal attack "uppercut" down+ab 
    Mizuki create new move jump light punch jump+a
    Mizuki create new move jump light  kick jump+c
    Mizuki create new move jump medium kick jump+d
    Mizuki create new move jump power  kick jump+cd
    Mizuki create new move stand light  kick
    Mizuki create new move stand medium kick
    Mizuki create new move stand power  kick
    Mizuki create new move down light  kick
    Mizuki create new move down medium kick
    Mizuki create new move down power  kick
    Mizuki create stun from attack hells circle
    Kuroko create stun from attack hells circle
    Wanfu readjusted 4 frames in recovery, move special "spirit blast massacre"
    Readjusted cross up all chars in the jump attacks
    Jubei readjusted 4 frames in recovery, move special "shingatou blocked counter"

--- src/burn/drv/neogeo/d_neogeo.cpp
+++ src/burn/drv/neogeo/d_neogeo.cpp
@@ -17684,9 +17684,9 @@ struct BurnDriver BurnDrvSamsho2sp = {
 // Samurai Shodown II Perfect Hack
 
 static struct BurnRomInfo samsho2peRomDesc[] = {
-	{ "063-p1pe.p1",	0x100000, 0x72a2a1de, 1 | BRF_ESS | BRF_PRG }, //  0 68K code
-	{ "063-p2pe.sp2",	0x100000, 0x998d61c7, 1 | BRF_ESS | BRF_PRG }, //  1
-	{ "063-p3pe.p3",	0x020000, 0xb5a452a3, 1 | BRF_ESS | BRF_PRG }, //  2 Extra ROM
+	{ "063-p1pe.p1",	0x100000, 0x8eb3dd7c, 1 | BRF_ESS | BRF_PRG }, //  0 68K code
+	{ "063-p2pe.sp2",	0x100000, 0x601902cb, 1 | BRF_ESS | BRF_PRG }, //  1
+	{ "063-p3pe.p3",	0x020000, 0x4ced821e, 1 | BRF_ESS | BRF_PRG }, //  2 Extra ROM
 
 	SAMSHO2_COMPONENT
 };

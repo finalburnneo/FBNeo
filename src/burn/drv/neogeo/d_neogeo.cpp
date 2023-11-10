394d628189e34e783f21ab162972878e3cdd871a
Igor-Arabe <110349306+IcDasNMs@users.noreply.github.com>
2023-11-10 19:30:24 -0300

    d_neogeo: samsho2pe 2nd commit
    .sp2 fix (new crc).

--- src/burn/drv/neogeo/d_neogeo.cpp
+++ src/burn/drv/neogeo/d_neogeo.cpp
@@ -17240,11 +17240,11 @@ struct BurnDriver BurnDrvSamsho2sp = {
 	0x1000, 320, 224, 4, 3
 };
 
-// Samurai Shodown II Perfect Hack v. 1.6 - 2023-11-09
+// Samurai Shodown II Perfect Hack v. 1.6 - 2023-11-10
 
 static struct BurnRomInfo samsho2peRomDesc[] = {
 	{ "063-p1pe.p1",	0x100000, 0x6d8f35d1, 1 | BRF_ESS | BRF_PRG }, //  0 68K code
-	{ "063-p2pe.sp2",	0x100000, 0x28f1a7bf, 1 | BRF_ESS | BRF_PRG }, //  1
+	{ "063-p2pe.sp2",	0x100000, 0x5cf930fe, 1 | BRF_ESS | BRF_PRG }, //  1
 	{ "063-p3pe.p3",	0x020000, 0xedffbd8a, 0 | BRF_ESS | BRF_PRG }, //  2 Extra ROM
 
 	{ "063-s1.s1",		0x020000, 0x64a5cd66, 2 | BRF_GRA },           //  3 Text layer tiles

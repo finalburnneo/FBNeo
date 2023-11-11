c5dcf7d9971b0334cb6719be73450831df5aa5f5
Igor-Arabe <110349306+IcDasNMs@users.noreply.github.com>
2023-11-11 01:42:22 -0300

    samsho2pe sp2 fix III
    Genjuro's Rekka 3rd bugfix.

--- src/burn/drv/neogeo/d_neogeo.cpp
+++ src/burn/drv/neogeo/d_neogeo.cpp
@@ -17244,7 +17244,7 @@ struct BurnDriver BurnDrvSamsho2sp = {
 
 static struct BurnRomInfo samsho2peRomDesc[] = {
 	{ "063-p1pe.p1",	0x100000, 0x6d8f35d1, 1 | BRF_ESS | BRF_PRG }, //  0 68K code
-	{ "063-p2pe.sp2",	0x100000, 0x80a32760, 1 | BRF_ESS | BRF_PRG }, //  1
+	{ "063-p2pe.sp2",	0x100000, 0x566dd5d5, 1 | BRF_ESS | BRF_PRG }, //  1
 	{ "063-p3pe.p3",	0x020000, 0xedffbd8a, 0 | BRF_ESS | BRF_PRG }, //  2 Extra ROM
 
 	{ "063-s1.s1",		0x020000, 0x64a5cd66, 2 | BRF_GRA },           //  3 Text layer tiles

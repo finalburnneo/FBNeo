9fd6ecb5a0f3aaf9fa41aa06c7cec7f3aef7e9cb
Igor-Arabe <110349306+IcDasNMs@users.noreply.github.com>
2024-01-11 13:06:50 -0300

    d_neogeo: mslug3cq
    updated to latest version (24-01-08).

--- src/burn/drv/neogeo/d_neogeo.cpp
+++ src/burn/drv/neogeo/d_neogeo.cpp
@@ -21331,8 +21331,8 @@ struct BurnDriver BurnDrvmslug31v2 = {
 // Modified by 合金弹头爱克斯
 
 static struct BurnRomInfo mslug3cqRomDesc[] = {
-	{ "256-ph1cq.p1",    0x100000, 0xa88e0cc5, 1 | BRF_ESS | BRF_PRG }, //  0 68K code
-	{ "256-ph2cq.sp2",   0x400000, 0x65d9fb62, 1 | BRF_ESS | BRF_PRG }, //  1 
+	{ "256-ph1cq.p1",    0x100000, 0x21d19ab7, 1 | BRF_ESS | BRF_PRG }, //  0 68K code
+	{ "256-ph2cq.sp2",   0x400000, 0xcbff6888, 1 | BRF_ESS | BRF_PRG }, //  1 
 
 	{ "256-c1d.c1",      0x800000, 0x3540398c, 3 | BRF_GRA },           //  2 Sprite data
 	{ "256-c2d.c2",      0x800000, 0xbdd220f0, 3 | BRF_GRA },           //  3
@@ -21340,8 +21340,8 @@ static struct BurnRomInfo mslug3cqRomDesc[] = {
 	{ "256-c4d.c4",      0x800000, 0x1463add6, 3 | BRF_GRA },           //  5
 	{ "256-c5d.c5",      0x800000, 0x48ca7f28, 3 | BRF_GRA },           //  6
 	{ "256-c6d.c6",      0x800000, 0x806eb36f, 3 | BRF_GRA },           //  7
-	{ "256-c7cq.c7",     0x800000, 0x84b3a1d0, 3 | BRF_GRA },           //  8
-	{ "256-c8cq.c8",     0x800000, 0xf7c16d82, 3 | BRF_GRA },           //  9
+	{ "256-c7cq.c7",     0x800000, 0x80e237ff, 3 | BRF_GRA },           //  8
+	{ "256-c8cq.c8",     0x800000, 0x39ee36c6, 3 | BRF_GRA },           //  9
 
 	{ "256-m1.m1",       0x080000, 0xeaeec116, 4 | BRF_ESS | BRF_PRG }, // 10 Z80 code
 

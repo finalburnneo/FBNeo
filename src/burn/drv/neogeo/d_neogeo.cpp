0469fcb8fba2dd0667effd6aee601b2e869fbdf0
Igor-Arabe <110349306+IcDasNMs@users.noreply.github.com>
2024-05-04 01:24:05 +0700

    d_neogeo: mslug5cq
    updated to latest version

--- src/burn/drv/neogeo/d_neogeo.cpp
+++ src/burn/drv/neogeo/d_neogeo.cpp
@@ -22072,11 +22072,11 @@ struct BurnDriver BurnDrvmslug5esr = {
 	0x1000,	304, 224, 4, 3
 };
 
-// Metal Slug 5 (The Ultimate Legend, Hack) - 2024-04-27
+// Metal Slug 5 (The Ultimate Legend, Hack) - 2024-05-02
 // Modified by 合金弹头爱克斯
 
 static struct BurnRomInfo mslug5cqRomDesc[] = {
-	{ "268-p1cq.p1",	0xa00000, 0x9fc88985, 1 | BRF_ESS | BRF_PRG },  //  0 68K code
+	{ "268-p1cq.p1",	0xa00000, 0x07723765, 1 | BRF_ESS | BRF_PRG },  //  0 68K code
 
 	{ "268-s1d.s1",		0x020000, 0x64952683, 2 | BRF_GRA },            //  1 Text layer tiles
 

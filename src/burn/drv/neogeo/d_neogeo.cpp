a9ba708b51d06b640223d3a0c41310b7d19034e5
Igor-Arabe <110349306+IcDasNMs@users.noreply.github.com>
2024-05-19 10:01:06 -0300

    d_neogeo: some changes
    updated kof94te [Igor-Arabe]
    added yoyoshkn [dinkc64]

--- src/burn/drv/neogeo/d_neogeo.cpp
+++ src/burn/drv/neogeo/d_neogeo.cpp
@@ -20320,6 +20320,37 @@ struct BurnDriver BurnDrvCyborgForce = {
 	0x1000, 320, 224, 4, 3
 };
 
+// YoYo-Shuriken (HB)
+// https://drludos.itch.io/yo-yo-shuriken-neo-geo
+
+static struct BurnRomInfo yoyoshknRomDesc[] = {
+	{ "yoyoshkn-p1.p1",    0x100000, 0x8a36bd7d, 1 | BRF_ESS | BRF_PRG }, //  0 68K code
+
+	{ "yoyoshkn-s1.s1",    0x020000, 0x59ce2b22, 2 | BRF_GRA },           //  1 Text layer tiles
+
+	{ "yoyoshkn-c1.c1",    0x200000, 0xfde9b178, 3 | BRF_GRA },           //  2 Sprite data
+	{ "yoyoshkn-c2.c2",    0x200000, 0x780c2045, 3 | BRF_GRA },           //  3
+
+	{ "yoyoshkn-m1.m1",    0x020000, 0x079ac27e, 4 | BRF_ESS | BRF_PRG }, //  4 Z80 code
+
+	{ "yoyoshkn-v1.v1",    0x200000, 0x79e1b73f, 5 | BRF_SND },           //  5 Sound data
+	{ "yoyoshkn-v2.v2",    0x200000, 0x4c464d41, 5 | BRF_SND },           //  6
+	{ "yoyoshkn-v3.v3",    0x200000, 0x0fbe1f09, 5 | BRF_SND },           //  7
+};
+
+STDROMPICKEXT(yoyoshkn, yoyoshkn, neogeo)
+STD_ROM_FN(yoyoshkn)
+
+struct BurnDriver BurnDrvYoyoshkn = {
+	"yoyoshkn", NULL, "neogeo", NULL, "2023",
+	"Yo-Yo Shuriken (HB)\0", NULL, "LudoSience", "Neo Geo MVS",
+	L"\u30e8\u30fc\u30e8\u30fc\u624b\u88cf\u5263\0YoYo-Shuriken (HB)\0", NULL, NULL, NULL,
+	BDF_GAME_WORKING | BDF_HOMEBREW, 2, HARDWARE_PREFIX_CARTRIDGE | HARDWARE_SNK_NEOGEO, GBF_SHOOT, 0,
+	NULL, yoyoshknRomInfo, yoyoshknRomName, NULL, NULL, NULL, NULL, neogeoInputInfo, neogeoDIPInfo,
+	NeoInit, NeoExit, NeoFrame, NeoRender, NeoScan, &NeoRecalcPalette,
+	0x1000,	304, 224, 4, 3
+};
+
 // Metal Slug Unity (Added Timer)
 // Modified by Alice愛麗絲/合金弹头爱克斯/CXZInc
 
@@ -20736,12 +20767,11 @@ struct BurnDriver BurnDrvkf2k3ps2sp = {
 	0x1000,	304, 224, 4, 3
 };
 
-// The King of Fighters '94 (Team Edit v1.0.0b4, Hack)
-// Modified by Matt Greer
-// source: https://neorh.mattgreer.dev/
+// King of Fighters '94 (Team Edit Edition v1.0.0, Hack)
+// https://neorh.mattgreer.dev/
 
 static struct BurnRomInfo kof94teRomDesc[] = {
-	{ "055-p1te.p1",	0x200000, 0xe0e334b1, 1 | BRF_ESS | BRF_PRG }, //  0 68K code
+	{ "055-p1te.p1",	0x200000, 0x3c44297a, 1 | BRF_ESS | BRF_PRG }, //  0 68K code
 
 	{ "055-s1te.s1",	0x020000, 0xdcd024d2, 2 | BRF_GRA },           //  1 Text layer tiles
 
@@ -20766,7 +20796,7 @@ STD_ROM_FN(kof94te)
 
 struct BurnDriver BurnDrvKof94te = {
 	"kof94te", "kof94", "neogeo", NULL, "2024",
-	"The King of Fighters '94 Team Edit (v1.0.0 beta 4, Hack)\0", NULL, "hack", "Neo Geo MVS",
+	"The King of Fighters '94 (Team Edit Edition v1.0.0, Hack)\0", NULL, "hack (Matt Greer)", "Neo Geo MVS",
 	NULL, NULL, NULL, NULL,
 	BDF_GAME_WORKING | BDF_CLONE | BDF_HACK | BDF_HISCORE_SUPPORTED, 2, HARDWARE_PREFIX_CARTRIDGE | HARDWARE_SNK_NEOGEO | HARDWARE_SNK_SWAPP, GBF_VSFIGHT, FBF_KOF,
 	NULL, kof94teRomInfo, kof94teRomName, NULL, NULL, NULL, NULL, neogeoInputInfo, neogeoDIPInfo,

313fdd5d516abf673e2e8482053158580bec5e41
dinkc64 <terra79pin@yahoo.com>
2024-06-19 01:26:46 -0400

    kaillera, synchronize for 60 frames before starting emulation, should fix games booting into weird modes

--- src/burner/win32/fba_kaillera.cpp
+++ src/burner/win32/fba_kaillera.cpp
@@ -1,12 +1,20 @@
 #include "burner.h"
 
+// history
+//
+// added sync frames on connect: the first 60 frames are tossed out.
+// - this fixes, for example: games booting into weird modes, sometimes
+//   the first few packets are null until the connections for all players
+//   stabilize.  -dink june 18, 2024
+
 const int MAXPLAYER = 4;
 static int nPlayerInputs[MAXPLAYER], nCommonInputs, nDIPInputs;
 static int nPlayerOffset[MAXPLAYER], nCommonOffset, nDIPOffset;
 
 const int INPUTSIZE = 8 * (4 + 8 + 0x40); // + 0x40 ZX Spectrum keyboard
 static unsigned char nControls[INPUTSIZE];
-
+static int nSyncFrame = 0;
+static unsigned char nKailleraFrameNum = 0;
 // Inputs are assumed to be in the following order:
 // All player 1 controls
 // All player 2 controls (if any)
@@ -17,6 +25,8 @@ static unsigned char nControls[INPUTSIZE];
 
 int KailleraInitInput()
 {
+	nSyncFrame = 60;
+
 	if (nGameInpCount == 0) {
 		return 1;
 	}
@@ -67,9 +77,12 @@ int KailleraInitInput()
 	return 0;
 }
 
+#define DEBUG_PACKETS 0
+
 int KailleraGetInput()
 {
 	int i, j, k;
+	int debug_packet_offset = 0;
 
 	struct BurnInputInfo bii;
 	memset(&bii, 0, sizeof(bii));
@@ -77,8 +90,15 @@ int KailleraGetInput()
 	// Initialize controls to 0
 	memset(nControls, 0, INPUTSIZE);
 
+#if DEBUG_PACKETS == 1
+	nKailleraFrameNum &= 0xff;
+	if (nKailleraFrameNum == 0x00) nKailleraFrameNum = 0x10;
+	nControls[0] = nKailleraFrameNum;
+	debug_packet_offset = 8;
+#endif
+
 	// Pack all player 1 controls, common controls, analog controls & DIP switches
-	for (i = 0, j = 0; i < nPlayerInputs[0]; i++, j++) {
+	for (i = 0, j = debug_packet_offset; i < nPlayerInputs[0]; i++, j++) {
 		BurnDrvGetInputInfo(&bii, i + nPlayerOffset[0]);
 		if (bii.pVal && *bii.pVal && bii.nType == BIT_DIGITAL) {
 			nControls[j >> 3] |= (1 << (j & 7));
@@ -122,14 +142,30 @@ int KailleraGetInput()
 	// k has the size of all inputs for one player
 	k = j + 1;
 
+	int nControlCount = 0;
 	// Send the control block to the Kaillera DLL & retrieve all controls
-	if (Kaillera_Modify_Play_Values(nControls, k) == -1) {
+	if ((nControlCount = Kaillera_Modify_Play_Values(nControls, k)) == -1) {
 		kNetGame = 0;
 		return 1;
 	}
 
+#if DEBUG_PACKETS == 1
+	for (int zzz = 0; zzz < nControlCount; zzz++) {
+		bprintf(0, _T("%02x"), nControls[zzz]);
+	}
+	bprintf(0, _T("%s\n"), (nSyncFrame > 0) ? _T(" - SYNC") : _T(""));
+#endif
+
+	{ // sync process!
+		if (nSyncFrame) {
+			nSyncFrame--;
+			nKailleraFrameNum = 0;
+			return 0;
+		}
+	}
+
 	// Decode Player 1 input block
-	for (i = 0, j = 0; i < nPlayerInputs[0]; i++, j++) {
+	for (i = 0, j = debug_packet_offset; i < nPlayerInputs[0]; i++, j++) {
 		BurnDrvGetInputInfo(&bii, i + nPlayerOffset[0]);
 		if (bii.nType == BIT_DIGITAL) {
 			if (nControls[j >> 3] & (1 << (j & 7))) {
@@ -170,13 +206,12 @@ int KailleraGetInput()
 	for (i = 0; i < nDIPInputs; i++, j++) {
 		BurnDrvGetInputInfo(&bii, i + nDIPOffset);
 		if (bii.pVal) *bii.pVal = nControls[j];
-		//bprintf(0, _T("remote dip %d:   %x\n"), i, nControls[j]);
 	}
 
 	// Decode other player's input blocks
 	for (int l = 1; l < MAXPLAYER; l++) {
 		if (nPlayerInputs[l]) {
-			for (i = 0, j = k * (l << 3); i < nPlayerInputs[l]; i++, j++) {
+			for (i = 0, j = k * ((l+(l*debug_packet_offset)) << 3); i < nPlayerInputs[l]; i++, j++) {
 				BurnDrvGetInputInfo(&bii, i + nPlayerOffset[l]);
 				if (bii.nType == BIT_DIGITAL) {
 					if (nControls[j >> 3] & (1 << (j & 7))) {
@@ -213,16 +248,10 @@ int KailleraGetInput()
 					}
 				}
 			}
-
-#if 0
-			// For a DIP switch to be set to 1, ALL players must set it
-			for (i = 0; i < nDIPInputs; i++, j++) {
-				BurnDrvGetInputInfo(&bii, i + nDIPOffset);
-				*bii.pVal &= nControls[j];
-			}
-#endif
 		}
 	}
+
+	nKailleraFrameNum++;
 
 	return 0;
 }

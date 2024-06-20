5c353c5334120dd69abaa4713c1747688f1db1a7
Igor-Arabe <110349306+IcDasNMs@users.noreply.github.com>
2024-06-19 21:25:56 -0300

    d_neogeo: kof94te
    update to 1.3.0 version (a95). [Igor-Arabe]

--- src/burn/drv/neogeo/d_neogeo.cpp
+++ src/burn/drv/neogeo/d_neogeo.cpp
@@ -20767,16 +20767,16 @@ struct BurnDriver BurnDrvkf2k3ps2sp = {
 	0x1000,	304, 224, 4, 3
 };
 
-// King of Fighters '94 (Team Edit Edition v1.2.4, Hack)
+// King of Fighters '94 (Team Edit Edition v1.3.0, Hack)
 // https://neorh.mattgreer.dev/
 
 static struct BurnRomInfo kof94teRomDesc[] = {
-	{ "055-p1te.p1",	0x200000, 0x18cefb28, 1 | BRF_ESS | BRF_PRG }, //  0 68K code
+	{ "055-p1te.p1",	0x200000, 0xf3fea336, 1 | BRF_ESS | BRF_PRG }, //  0 68K code
 
 	{ "055-s1te.s1",	0x020000, 0xdcd024d2, 2 | BRF_GRA },           //  1 Text layer tiles
 
-	{ "055-c1te.c1",	0x200000, 0x9fc6044e, 3 | BRF_GRA },           //  2 Sprite data
-	{ "055-c2te.c2",	0x200000, 0xc5e4a59e, 3 | BRF_GRA },           //  3
+	{ "055-c1te.c1",	0x200000, 0x34758352, 3 | BRF_GRA },           //  2 Sprite data
+	{ "055-c2te.c2",	0x200000, 0x90466a40, 3 | BRF_GRA },           //  3
 	{ "055-c3.c3",		0x200000, 0x54f66254, 3 | BRF_GRA },           //  4
 	{ "055-c4.c4",		0x200000, 0x0b01765f, 3 | BRF_GRA },           //  5
 	{ "055-c5.c5",		0x200000, 0xee759363, 3 | BRF_GRA },           //  6
@@ -20796,7 +20796,7 @@ STD_ROM_FN(kof94te)
 
 struct BurnDriver BurnDrvKof94te = {
 	"kof94te", "kof94", "neogeo", NULL, "2024",
-	"The King of Fighters '94 (Team Edit Edition v1.2.4, Hack)\0", NULL, "hack (Matt Greer)", "Neo Geo MVS",
+	"The King of Fighters '94 (Team Edit Edition v1.3.0, Hack)\0", NULL, "hack (Matt Greer)", "Neo Geo MVS",
 	NULL, NULL, NULL, NULL,
 	BDF_GAME_WORKING | BDF_CLONE | BDF_HACK | BDF_HISCORE_SUPPORTED, 2, HARDWARE_PREFIX_CARTRIDGE | HARDWARE_SNK_NEOGEO | HARDWARE_SNK_SWAPP, GBF_VSFIGHT, FBF_KOF,
 	NULL, kof94teRomInfo, kof94teRomName, NULL, NULL, NULL, NULL, neogeoInputInfo, neogeoDIPInfo,

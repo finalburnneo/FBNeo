e6016712a0080f08c2c34b101d52adb822a6c9dc
barbudreadmon <barbudreadmon@gmail.com>
2024-09-18 10:44:24 +0200

    midway: resolution shenanigans

--- src/burn/drv/midway/midwunit.h
+++ src/burn/drv/midway/midwunit.h
@@ -12,6 +12,15 @@ extern UINT8 nWolfUnitRecalc;
 #define WUNIT_GFX_ADR(value)	((value & (0xFF << 12)) >> 12)
 #define WUNIT_GFX_OFF(value)	((value & (7 << 8)) >> 8)
 
+#define WUNIT_SCREEN_WIDTH  400
+// standalone builds have an issue where filters don't work properly
+// if height is not divisible by 4
+#ifndef __LIBRETRO__
+#define WUNIT_SCREEN_HEIGHT 256
+#else
+#define WUNIT_SCREEN_HEIGHT 254
+#endif
+
 INT32 WolfUnitInit();
 INT32 WolfUnitFrame();
 INT32 WolfUnitExit();

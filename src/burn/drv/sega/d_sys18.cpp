887d5485d2c1e1f2e1d8de5629b84c565fd9d59a
dinkc64 <terra79pin@yahoo.com>
2024-12-23 09:34:24 -0500

    mwalku, fix coin

--- src/burn/drv/sega/d_sys18.cpp
+++ src/burn/drv/sega/d_sys18.cpp
@@ -845,6 +845,51 @@ static struct BurnDIPInfo MwalkDIPList[]=
 
 STDDIPINFO(Mwalk)
 
+// same as above, except Coin Chute & Play Mode swapped bits
+static struct BurnDIPInfo MwalkuDIPList[]=
+{
+	DIP_OFFSET(0x1e)
+	// Default Values
+	{0x00, 0xff, 0xff, 0xff, NULL                                 },
+	{0x01, 0xff, 0xff, 0xfd, NULL                                 },
+
+	// Dip 1
+	SYSTEM18_COINAGE(0x00)
+
+	// Dip 2
+	{0   , 0xfe, 0   , 2   , "2 Credits to Start"                 },
+	{0x01, 0x01, 0x01, 0x01, "Off"                                },
+	{0x01, 0x01, 0x01, 0x00, "On"                                 },
+
+	{0   , 0xfe, 0   , 2   , "Demo Sounds"                        },
+	{0x01, 0x01, 0x02, 0x02, "Off"                                },
+	{0x01, 0x01, 0x02, 0x00, "On"                                 },
+
+	{0   , 0xfe, 0   , 2   , "Lives"                              },
+	{0x01, 0x01, 0x04, 0x04, "2"                                  },
+	{0x01, 0x01, 0x04, 0x00, "3"                                  },
+
+	{0   , 0xfe, 0   , 2   , "Player Vitality"                    },
+	{0x01, 0x01, 0x08, 0x08, "Low"                                },
+	{0x01, 0x01, 0x08, 0x00, "High"                               },
+
+	{0   , 0xfe, 0   , 2   , "Play Mode"                          },
+	{0x01, 0x01, 0x10, 0x00, "2 Players"                          },
+	{0x01, 0x01, 0x10, 0x10, "3 Players"                          },
+
+	{0   , 0xfe, 0   , 2   , "Coin Chute"                         },
+	{0x01, 0x01, 0x20, 0x00, "Common"                             },
+	{0x01, 0x01, 0x20, 0x20, "Individual"                         },
+
+	{0   , 0xfe, 0   , 4   , "Difficulty"                         },
+	{0x01, 0x01, 0xc0, 0x80, "Easy"                               },
+	{0x01, 0x01, 0xc0, 0xc0, "Normal"                             },
+	{0x01, 0x01, 0xc0, 0x40, "Hard"                               },
+	{0x01, 0x01, 0xc0, 0x00, "Hardest"                            },
+};
+
+STDDIPINFO(Mwalku)
+
 static void MwalkFixInputs()
 {
 	// Depending on dips, coin changes
@@ -4031,8 +4076,8 @@ struct BurnDriver BurnDrvMWalku = {
 	"Michael Jackson's Moonwalker (US) (FD1094/8751 317-0158)\0", NULL, "Sega", "System 18",
 	NULL, NULL, NULL, NULL,
 	BDF_GAME_WORKING | BDF_CLONE, 3, HARDWARE_SEGA_SYSTEM18 | HARDWARE_SEGA_FD1094_ENC | HARDWARE_SEGA_171_5874, GBF_PLATFORM, 0,
-	NULL, MwalkuRomInfo, MwalkuRomName, NULL, NULL, NULL, NULL, MwalkInputInfo, MwalkDIPInfo,
-	MwalkInit, System18Exit, System18Frame, System18Render, System18Scan,
+	NULL, MwalkuRomInfo, MwalkuRomName, NULL, NULL, NULL, NULL, MwalkInputInfo, MwalkuDIPInfo,
+	MwalkudInit, System18Exit, System18Frame, System18Render, System18Scan,
 	NULL, 0x1800, 320, 224, 4, 3
 };
 

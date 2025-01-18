d593bb9c431a268f46e32a71cda9d8f0b9f52bc5
dinkc64 <terra79pin@yahoo.com>
2025-01-18 17:53:15 -0500

    add neocd shinobi to the list

--- src/burner/win32/neocdlist.cpp
+++ src/burner/win32/neocdlist.cpp
@@ -150,6 +150,7 @@ struct NGCDGAME games[] =
 	{ _T("sbp")			, _T("Super Bubble Pop")									, _T("2024")	, _T("Vektorlogic (iq_132 conversion)")	, 0xfedc },		//
 	{ _T("zupapa")		, _T("Zupapa!")												, _T("2024")	, _T("SNK (iq_132 conversion)")	, 0x0070 },		//
 	{ _T("zedblade")	, _T("Zed Blade - Operation Ragnarok")						, _T("2024")	, _T("NMK (iq_132 conversion)")	, 0x0076 },		//
+	{ _T("shinobi")		, _T("Shinobi")												, _T("2024")	, _T("(Hoffman conversion)")	, 0x1337 },		//
 	{ _T("cbarrel")		, _T("Captain Barrel")										, _T("2024")	, _T("Ozzy Ouzo)")	, 0x14A1 },		//
 };
 

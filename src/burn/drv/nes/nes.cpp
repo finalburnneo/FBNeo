224f46b3962e648b8b0eabdb1c5c41a6bfe57f43
dinkc64 <terra79pin@yahoo.com>
2025-03-01 20:42:05 -0500

    update famidash [lolsuite]

--- src/burn/drv/nes/nes.cpp
+++ src/burn/drv/nes/nes.cpp
@@ -494,7 +494,8 @@ static INT32 cartridge_load(UINT8* ROMData, UINT32 ROMSize, UINT32 ROMCRC)
 	NESMode |= (ROMCRC == 0xb90a1ca1) ? NO_WORKRAM : 0; // Low G Man
 	NESMode |= (ROMCRC == 0xa905cc12) ? NO_WORKRAM : 0; // Bill & Ted
 	NESMode |= (ROMCRC == 0x45b1869a) ? APU_HACKERY : 0; // rgbleek
-	NESMode |= (ROMCRC == 0x9cc0ee5b) ? APU_HACKERY : 0; // famidash
+	NESMode |= (ROMCRC == 0x708fbee4) ? APU_HACKERY : 0; // famidash
+	NESMode |= (ROMCRC == 0x2f19e44b) ? APU_HACKERY : 0; // famidashb
 	NESMode |= (ROMCRC == 0xc00c4ea5) ? APU_HACKERY : 0; // Sam's Journey
 	NESMode |= (ROMCRC == 0x585f3500) ? ALT_MMC3 : 0; // Darkwing Duck (T-Chi)
 	NESMode |= (ROMCRC == 0x38f65b2d) ? BAD_HOMEBREW : 0; // Battler (HB)

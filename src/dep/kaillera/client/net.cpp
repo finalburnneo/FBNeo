ef16b405c8367d1db75adca0f813813e82e90db6
taoenwen <taoenwen@gmail.com>
2025-05-24 22:05:22 +0800

    win32 kailleraclient: simplified search

--- src/dep/kaillera/client/net.cpp
+++ src/dep/kaillera/client/net.cpp
@@ -59,55 +59,6 @@ int WINAPI Empty_Kaillera_End_Game()
 	return 0;
 }
 
-static UINT_PTR CALLBACK KailleraDllHook(HWND hDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
-{
-	if (uMsg == CDN_FILEOK) {
-		OPENFILENAMEA* pOFN = (OPENFILENAMEA*)lParam;
-
-		char szPath[MAX_PATH] = { 0 };
-		strcpy(szPath, pOFN->lpstrFile);
-
-		char szFile[MAX_PATH] = { 0 };
-		char* pszFileName = szPath, * pLastSlash = NULL;
-
-		while ('\0' != *pszFileName) {
-			if ('\\' == *pszFileName) {
-				pLastSlash = pszFileName;
-			}
-			pszFileName++;
-		}
-
-		if (NULL != pLastSlash) strcpy(szFile, pLastSlash + 1);
-		else                    strcpy(szFile, szPath);
-
-		if (0 != strcmp(szFile, "kailleraclient.dll")); {
-			MessageBoxA(hDlg, "Only the file named \'kailleraclient.dll\' can be selected.", "Error", MB_ICONERROR);
-			return TRUE;
-		}
-	}
-	return FALSE;
-}
-
-static HMODULE FindkailleraDll()
-{
-	char szFile[MAX_PATH] = "kailleraclient.dll";
-	OPENFILENAMEA ofn     = { 0 };
-	ofn.lStructSize       = sizeof(OPENFILENAMEA);
-	ofn.hwndOwner         = NULL;
-	ofn.lpstrFilter       = "kailleraclient.dll\0kailleraclient.dll\0\0";
-	ofn.lpstrFile         = szFile;
-	ofn.nMaxFile          = MAX_PATH;
-	ofn.Flags             = OFN_EXPLORER | OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
-	ofn.lpfnHook          = KailleraDllHook;
-	ofn.lpstrInitialDir   = ".";
-
-	if (FALSE != GetOpenFileNameA(&ofn)) {
-		return LoadLibraryA(szFile);
-	}
-	return NULL;
-}
-
-
 int Init_Network(void)
 {
 #if 0
@@ -119,9 +70,6 @@ int Init_Network(void)
 #endif // 0
 
 #ifdef BUILD_X64_EXE
-#define	KailleraClientDll			"kailleraclient64.dll"
-#define	AlternativeDll				"kailleraclient\\x64\\kailleraclient.dll"
-#define	KailleraPeError				"Please select the 64-bit version of kailleraclient.dll"
 #define	kailleraGetVersion			"kailleraGetVersion"
 #define	kailleraInit				"kailleraInit"
 #define	kailleraShutdown			"kailleraShutdown"
@@ -131,9 +79,6 @@ int Init_Network(void)
 #define	kailleraChatSend			"kailleraChatSend"
 #define	kailleraEndGame				"kailleraEndGame"
 #else
-#define	KailleraClientDll			"kailleraclient.dll"
-#define	AlternativeDll				"kailleraclient\\x86\\kailleraclient.dll"
-#define	KailleraPeError				"Please select the 32-bit version of kailleraclient.dll"
 #define	kailleraGetVersion			"_kailleraGetVersion@4"
 #define	kailleraInit				"_kailleraInit@0"
 #define	kailleraShutdown			"_kailleraShutdown@0"
@@ -144,19 +89,11 @@ int Init_Network(void)
 #define	kailleraEndGame				"_kailleraEndGame@0"
 #endif
 
-	Kaillera_HDLL = LoadLibraryA(KailleraClientDll);
+	Kaillera_HDLL = LoadLibraryA("kailleraclient.dll");
 
-	if (Kaillera_HDLL == NULL) {
-		Kaillera_HDLL = LoadLibraryA(AlternativeDll);	// 1st
-	}
-	// 
-	if (Kaillera_HDLL == NULL) {
-		Kaillera_HDLL = FindkailleraDll();				// 2nd
-	}
-	if ((Kaillera_HDLL == NULL) && (ERROR_BAD_EXE_FORMAT == GetLastError())) {
-		if (IDRETRY == MessageBoxA(NULL, KailleraPeError, "Error", MB_RETRYCANCEL | MB_ICONERROR)) {
-			Kaillera_HDLL = FindkailleraDll();			// 3rd
-		}
+	if ((Kaillera_HDLL == NULL) || (ERROR_BAD_EXE_FORMAT == GetLastError())) {
+		Kaillera_HDLL = LoadLibraryA("kailleraclient64.dll");
+
 	}
 	
 	if (Kaillera_HDLL != NULL)

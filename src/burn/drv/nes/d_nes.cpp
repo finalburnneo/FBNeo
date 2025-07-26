e1003acf411f5fe9e18745f323ecf9f0cd2096fb
taoenwen <67533945+taoenwen@users.noreply.github.com>
2025-07-26 09:34:59 +0800

    d_nes: update Celeste Mario's Zap & Dash DELUXE! v2.00c (Hack)
    v2.00 -> v2.00c

--- src/burn/drv/nes/d_nes.cpp
+++ src/burn/drv/nes/d_nes.cpp
@@ -14176,10 +14176,10 @@ struct BurnDriver BurnDrvnes_celestemario = {
 	SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_WIDTH, SCREEN_HEIGHT
 };
 
-// Celeste Mario's Zap & Dash DELUXE! v2.00 (Hack)
+// Celeste Mario's Zap & Dash DELUXE! v2.00c (Hack)
 // https://www.romhacking.net/hacks/9044/
 static struct BurnRomInfo nes_celestemariodlRomDesc[] = {
-	{ "Celeste Mario's Zap & Dash DELUXE! v2.00 (2025)(w7n).nes",          655376, 0x3a8afc37, BRF_ESS | BRF_PRG },
+	{ "Celeste Mario's Zap & Dash DELUXE! v2.00c (2025)(w7n).nes",          655376, 0x387485d3, BRF_ESS | BRF_PRG },
 };
 
 STD_ROM_PICK(nes_celestemariodl)
@@ -14187,7 +14187,7 @@ STD_ROM_FN(nes_celestemariodl)
 
 struct BurnDriver BurnDrvnes_celestemariodl = {
 	"nes_celestemariodl", "nes_smb", NULL, NULL, "2025",
-	"Celeste Mario's Zap & Dash DELUXE! v2.00 (Hack)\0", NULL, "w7n", "NES / Famicom",
+	"Celeste Mario's Zap & Dash DELUXE! v2.00c (Hack)\0", NULL, "w7n", "NES / Famicom",
 	NULL, NULL, NULL, NULL,
 	BDF_GAME_WORKING | BDF_CLONE | BDF_HACK, 1, HARDWARE_NES, GBF_PLATFORM, 0,
 	NESGetZipName, nes_celestemariodlRomInfo, nes_celestemariodlRomName, NULL, NULL, NULL, NULL, NESInputInfo, NESDIPInfo,

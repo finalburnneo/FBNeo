289a3113814536961f0c108fb8c0fa234a137502
dinkc64 <terra79pin@yahoo.com>
2025-07-29 20:39:35 -0400

    mbtwc.cpp, fix cross compile (name case)

--- src/burner/win32/mbtwc.cpp
+++ src/burner/win32/mbtwc.cpp
@@ -1,7 +1,7 @@
 #include "mbtwc.h"
 
 #define WIN32_LEAN_AND_MEAN
-#include <Windows.h>
+#include <windows.h>
 
 int _MultiByteToWideChar(unsigned int CodePage, unsigned long dwFlags, const char* lpMultiByteStr, int cbMultiByte, wchar_t* lpWideCharStr, int cchWideChar) {
 	return MultiByteToWideChar(CodePage, dwFlags, lpMultiByteStr, cbMultiByte, lpWideCharStr, cchWideChar);

3c4d97f145516f30582a688e2c0daa92d70aa901
dinkc64 <terra79pin@yahoo.com>
2026-04-25 02:13:53 -0400

    megadrive update from gab75

--- src/burn/drv/megadrive/d_megadrive.cpp
+++ src/burn/drv/megadrive/d_megadrive.cpp
@@ -44022,10 +44022,10 @@ struct BurnDriver BurnDrvmd_ghostbstse = {
 	&bMegadriveRecalcPalette, 0x100, 320, 224, 4, 3
 };
 
-// Golden Axe - Arcade Colors (Hack, v1.2)
+// Golden Axe - Arcade Colors (Hack, v1.3)
 // https://www.romhacking.net/hacks/7004/
 static struct BurnRomInfo md_goldnaxeacRomDesc[] = {
-	{ "Golden Axe - Arcade Colors v1.2 (2026)(SCD).bin", 590842, 0xa14e80f7, BRF_PRG | SEGA_MD_ROM_LOAD16_WORD_SWAP | SEGA_MD_ROM_OFFS_000000  },
+	{ "Golden Axe - Arcade Colors v1.3 (2026)(SCD).bin", 590842, 0x0f0f9154, BRF_PRG | SEGA_MD_ROM_LOAD16_WORD_SWAP | SEGA_MD_ROM_OFFS_000000  },
 };
 
 STD_ROM_PICK(md_goldnaxeac)
@@ -44033,7 +44033,7 @@ STD_ROM_FN(md_goldnaxeac)
 
 struct BurnDriver BurnDrvmd_goldnaxeac = {
 	"md_goldnaxeac", "md_goldnaxe", NULL, NULL, "2026",
-	"Golden Axe - Arcade Colors (Hack, v1.2)\0", NULL, "SCD", "Genesis / Mega Drive",
+	"Golden Axe - Arcade Colors (Hack, v1.3)\0", NULL, "SCD", "Genesis / Mega Drive",
 	NULL, NULL, NULL, NULL,
 	BDF_GAME_WORKING | BDF_16BIT_ONLY | BDF_CLONE | BDF_HACK, 2, HARDWARE_SEGA_MEGADRIVE, GBF_SCRFIGHT, 0,
 	MegadriveGetZipName, md_goldnaxeacRomInfo, md_goldnaxeacRomName, NULL, NULL, NULL, NULL, MegadriveInputInfo, MegadriveDIPInfo,

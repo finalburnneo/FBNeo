56f0557b3bfb93d080fb80dd60dc2ce0ad158f07
barbudreadmon <barbudreadmon@users.noreply.github.com>
2026-05-25 16:23:22 +0200

    add atarisy1 to REDUCE_TOTAL_SETS_BIOS count

--- src/burner/win32/sel.cpp
+++ src/burner/win32/sel.cpp
@@ -3,9 +3,9 @@
 #include "burner.h"
 #include <process.h>
 
-// reduce the total number of sets by this number - (isgsm, neogeo, nmk004, pgm, skns, ym2608, coleco, msx_msx, spectrum, spec128, spec1282a, decocass, midssio, cchip, fdsbios, ngp, bubsys, channelf, namcoc69, namcoc70, namcoc75, snes stuff)
+// reduce the total number of sets by this number - (isgsm, neogeo, nmk004, pgm, skns, ym2608, coleco, msx_msx, spectrum, spec128, spec1282a, decocass, midssio, cchip, fdsbios, ngp, bubsys, channelf, namcoc69, namcoc70, namcoc75, atarisy1, snes stuff)
 // don't reduce for these as we display them in the list (neogeo, neocdz)
-#define REDUCE_TOTAL_SETS_BIOS      28
+#define REDUCE_TOTAL_SETS_BIOS      29
 
 UINT_PTR nTimer					= 0;
 UINT_PTR nInitPreviewTimer		= 0;

25f42d19eec74d13e4d320fe3993bb2b0cdcb0d8
JacKc029735 <37247884+JacKc029735@users.noreply.github.com>
2026-06-22 21:52:52 +0200

    d_cps2.cpp: Update  Street Fighter Zero 3 Mix to v0.32 [Zero800]
    - Thanks to Jets from "https://www.emu-france.com/" for letting me know.

--- src/burn/drv/capcom/d_cps2.cpp
+++ src/burn/drv/capcom/d_cps2.cpp
@@ -14044,7 +14044,7 @@ static struct BurnRomInfo Sfz3jr2dRomDesc[] = {
 STD_ROM_PICK(Sfz3jr2d)
 STD_ROM_FN(Sfz3jr2d)
 
-// Street Fighter Zero 3 Mix v0.31 (hacked by Zero800)
+// Street Fighter Zero 3 Mix v0.32 (hacked by Zero800)
 // https://sfz3mix.github.io/
 
 static struct BurnSampleInfo Sfz3mixSampleDesc[] = {
@@ -14183,9 +14183,9 @@ STD_SAMPLE_PICK(Sfz3mix)
 STD_SAMPLE_FN(Sfz3mix)
 
 static struct BurnRomInfo Sfz3mixRomDesc[] = {
-	{ "sz3mx.03",		0x0600000, 0x94c8f969, CPS2_PRG_68K | BRF_ESS | BRF_PRG },
+	{ "sz3mx.03",		0x0600000, 0x7a968da6, CPS2_PRG_68K | BRF_ESS | BRF_PRG },
 
-	{ "sz3mx.13m",		0x2000000, 0x59025f91, CPS2_GFX | BRF_GRA },
+	{ "sz3mx.13m",		0x2000000, 0x5389b651, CPS2_GFX | BRF_GRA },
 	{ "sz3mx.21m",		0x2000000, 0x914c7031, CPS2_GFX | BRF_GRA },
 
 	{ "sz3mx.01",		0x0080000, 0x52f98a5c, CPS2_PRG_Z80 | BRF_ESS | BRF_PRG },
@@ -15213,8 +15213,8 @@ static INT32 Cps2TurboInit()
 }
 
 struct BurnDriver BurnDrvCpsSfz3mix = {
-	"sfz3mix", "sfa3", NULL, "sfz3mix", "2024",
-	"Street Fighter Zero 3 Mix v0.31\0", NULL, "hack (zero800)", "CPS2",
+	"sfz3mix", "sfa3", NULL, "sfz3mix", "2026",
+	"Street Fighter Zero 3 Mix v0.32\0", NULL, "hack (zero800)", "CPS2",
 	NULL, NULL, NULL, NULL,
 	BDF_GAME_WORKING | BDF_CLONE | BDF_HACK | BDF_HISCORE_SUPPORTED, 2, HARDWARE_CAPCOM_CPS2, GBF_VSFIGHT, FBF_SF,
 	NULL, Sfz3mixRomInfo, Sfz3mixRomName, NULL, NULL, Sfz3mixSampleInfo, Sfz3mixSampleName, Sfz3mixInputInfo, Sfz3mixDIPInfo,

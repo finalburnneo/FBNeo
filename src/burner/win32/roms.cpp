ec7dbb63a5e35c94ab5640dd4bc3903d15dc452e
fprietog <fprietog@hotmail.com>
2026-08-07 04:51:39 +0200

    Fix typo

--- src/burner/win32/roms.cpp
+++ src/burner/win32/roms.cpp
@@ -37,7 +37,7 @@ static const TCHAR szAppDefaultPaths[DIRS_MAX][MAX_PATH] = {
 	{ _T("roms/ngp/")		},
 	{ _T("roms/channelf/")	},
 	{ _T("roms/astrocade/")	},
-	{ _T("roms/gba/")	},
+	{ _T("roms/gba/")		},
 	{ _T("roms/romdata/")	},
 	{ _T("")				}
 };

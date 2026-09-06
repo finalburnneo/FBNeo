9733d1dc536ebdfc8e1f0424f75a7c360c931b68
fprietog <fprietog@hotmail.com>
2026-09-06 17:19:27 +0200

    d_gba.cpp: moved GBalatro to its correct place

--- src/burn/drv/gba/d_gba.cpp
+++ src/burn/drv/gba/d_gba.cpp
@@ -55944,25 +55944,6 @@ struct BurnDriver BurnDrvgba_battleaf = {
 	GBA_WIDTH, GBA_HEIGHT, 3, 2
 };
 
-// GBalatro (HB, v0.2.2)
-
-static struct BurnRomInfo gba_gbalatroRomDesc[] = {
-	{ "GBalatro v0.2.2 (2025)(Various).gba",	5061324,	0x008d8f50,	BRF_ESS | BRF_PRG },
-};
-
-STDROMPICKEXT(gba_gbalatro, gba_gbalatro, gba_gba)
-STD_ROM_FN(gba_gbalatro)
-
-struct BurnDriver BurnDrvgba_gbalatro = {
-	"gba_gbalatro", NULL, "gba_gba", NULL, "2025",
-	"GBalatro (HB, v0.2.2)\0", NULL, "Various", "Game Boy Advance",
-	NULL, NULL, NULL, NULL,
-	BDF_GAME_WORKING | BDF_HOMEBREW, 1, HARDWARE_PREFIX_CARTRIDGE | HARDWARE_GBA, GBF_CARD, 0,
-	GbaGetZipName, gba_gbalatroRomInfo, gba_gbalatroRomName, NULL, NULL, NULL, NULL, GbaInputInfo, GbaDIPInfo,
-	DrvInit, DrvExit, DrvFrame, DrvDraw, DrvScan, &DrvRecalc, 0,
-	GBA_WIDTH, GBA_HEIGHT, 3, 2
-};
-
 // Beat Beast (HB)
 
 static struct BurnRomInfo gba_beatbeastRomDesc[] = {
@@ -57098,6 +57079,25 @@ struct BurnDriver BurnDrvgba_galaxywall = {
 	NULL, NULL, NULL, NULL,
 	BDF_GAME_WORKING | BDF_HOMEBREW, 1, HARDWARE_PREFIX_CARTRIDGE | HARDWARE_GBA, GBF_PUZZLE, 0,
 	GbaGetZipName, gba_galaxywallRomInfo, gba_galaxywallRomName, NULL, NULL, NULL, NULL, GbaInputInfo, GbaDIPInfo,
+	DrvInit, DrvExit, DrvFrame, DrvDraw, DrvScan, &DrvRecalc, 0,
+	GBA_WIDTH, GBA_HEIGHT, 3, 2
+};
+
+// GBalatro (HB, v0.2.2)
+
+static struct BurnRomInfo gba_gbalatroRomDesc[] = {
+	{ "GBalatro v0.2.2 (2025)(Various).gba",	5061324,	0x008d8f50,	BRF_ESS | BRF_PRG },
+};
+
+STDROMPICKEXT(gba_gbalatro, gba_gbalatro, gba_gba)
+STD_ROM_FN(gba_gbalatro)
+
+struct BurnDriver BurnDrvgba_gbalatro = {
+	"gba_gbalatro", NULL, "gba_gba", NULL, "2025",
+	"GBalatro (HB, v0.2.2)\0", NULL, "Various", "Game Boy Advance",
+	NULL, NULL, NULL, NULL,
+	BDF_GAME_WORKING | BDF_HOMEBREW, 1, HARDWARE_PREFIX_CARTRIDGE | HARDWARE_GBA, GBF_CARD, 0,
+	GbaGetZipName, gba_gbalatroRomInfo, gba_gbalatroRomName, NULL, NULL, NULL, NULL, GbaInputInfo, GbaDIPInfo,
 	DrvInit, DrvExit, DrvFrame, DrvDraw, DrvScan, &DrvRecalc, 0,
 	GBA_WIDTH, GBA_HEIGHT, 3, 2
 };
